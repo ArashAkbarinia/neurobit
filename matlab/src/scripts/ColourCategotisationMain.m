0cadaffba3c369d9aa412412fab8e2e08384c020
Arash Akbarinia <akbarinia.arash@gmail.com>
2014-11-11 12:05:58 +0100

    macbeth colour checker function

--- matlab/src/scripts/ColourCategotisationMain.m
+++ matlab/src/scripts/ColourCategotisationMain.m
@@ -7,13 +7,6 @@ clc;
 ConfigsMat = load('2014_ellipsoid_params');
 ColourEllipsoids = ConfigsMat.ellipsoids;
 
-Macbeth = zeros(4, 6, 3); 
-Macbeth(:,:,1) = [115, 196,  93,  90, 130,  99; 220,  72, 195,  91, 160, 229;  43,  71, 176, 238, 188,   0; 245, 200, 160, 120, 83, 50];
-Macbeth(:,:,2) = [ 81, 149, 123, 108, 129, 191; 123,  92,  84,  59, 189, 161;  62, 149,  48, 200,  84, 136; 245, 201, 161, 121, 84, 50];
-Macbeth(:,:,3) = [ 67, 129, 157,  65, 176, 171;  45, 168,  98, 105,  62,  41; 147,  72,  56,  22, 150, 166; 240, 201, 161, 121, 85, 50];
-Macbeth = uint8(Macbeth);
-% ImageRGB = Macbeth;
-
 ImageRGB = imread('colorapp_pep.png');
 PlotResults = 1;
 [BelongingImage, ColouredBelongingImage] = RGB2ColourNaming(ImageRGB, ColourEllipsoids, PlotResults);
