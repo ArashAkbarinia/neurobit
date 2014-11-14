5efd67c71adc8eb38188a8ceda9d57a6aff6551c
Arash Akbarinia <akbarinia.arash@gmail.com>
2014-11-14 13:03:41 +0100

    grey world algorithm

---
+++ matlab/src/algorithms/colourconstancy/ColourConstancyGreyWorld.m
@@ -0,0 +1,32 @@
+function ColourConstantImage = ColourConstancyGreyWorld(InputImage, ScaleFactor)
+%ColourConstancyGreyWrold applies the grey world algorithm to the input.
+%   Explanation http://en.wikipedia.org/wiki/Color_normalization#Grey_world
+%
+% Inputs
+%   InputImage   the input image.
+%   ScaleFactor  the scale factor, default is average intensity value.
+%
+% Outputs
+%   ColourConstantImage  the colour constant image in range of [0, 1].
+%
+% Examples
+%   im = imread('peppers.png');
+%   imout = ColourConstancyGreyWorld(im);
+%   figure; imshow(uint8(imout .* 255));
+%
+% See also: ColourConstancy
+%
+
+InputImage = im2double(InputImage);
+
+immean = mean(mean(InputImage));
+
+if nargin < 2
+  ScaleFactor = mean(immean);
+end
+
+k = ScaleFactor / immean;
+
+ColourConstantImage = MatChansMulK(InputImage, k);
+
+end
