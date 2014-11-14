4b84a0e395ce121689ec983078953c76d9d3d896
Arash Akbarinia <akbarinia.arash@gmail.com>
2014-11-14 13:03:57 +0100

    white patch algorithm

---
+++ matlab/src/algorithms/colourconstancy/ColourConstancyWhitePatch.m
@@ -0,0 +1,25 @@
+function ColourConstantImage = ColourConstancyWhitePatch(InputImage)
+%ColourConstancyWhitePatch applies the white patch algorithm to the input.
+%
+% Inputs
+%   InputImage   the input image.
+%
+% Outputs
+%   ColourConstantImage  the colour constant image in range of [0, 1].
+%
+% Examples
+%   im = imread('peppers.png');
+%   imout = ColourConstancyWhitePatch(im);
+%   figure; imshow(uint8(imout .* 255));
+%
+% See also: ColourConstancy
+%
+
+InputImage = im2double(InputImage);
+
+immax = max(max(InputImage));
+k = 1.0 / immax;
+
+ColourConstantImage = MatChansMulK(InputImage, k);
+
+end
