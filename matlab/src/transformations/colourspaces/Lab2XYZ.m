8a06a89f7e479a83d5b9704b00ceea2764786123
Arash Akbarinia <akbarinia.arash@gmail.com>
2014-12-16 09:19:22 +0100

    changed Lab2XYZ to matlab code

--- matlab/src/transformations/colourspaces/Lab2XYZ.m
+++ matlab/src/transformations/colourspaces/Lab2XYZ.m
@@ -1,108 +1,15 @@
-function XYZ = Lab2XYZ(Lab, XYZmatrix)
+function xyz = Lab2XYZ(lab, WhiteReference)
 % XYZ = Lab2XYZ(Lab,whiteXYZ)
 %
 % Convert Lab to XYZ, given the XYZ coordinates of
 % the white point.
 %
-% 10/10/93    dhb   Converted from CAP C code.
-% 5/9/02      dhb   Improved help
-% Modified by Alej from DHB's Psychotoolbox code
-%===============================================
-% if nargin < 2
-%     params = InitBits('LinearLUT');
-%     XYZmatrix = params{2};
-% end
 
 if nargin < 2
-  whiteXYZ = [0.950170; 1.000000; 1.088130];
-  %White point of D65
-elseif isnumeric(XYZmatrix)
-  whiteXYZ = XYZmatrix';
-else
-  whiteXYZ = XYZmatrix.white'; %for backward compatibility!!!
+  % white point of D65
+  WhiteReference = [0.950170, 1.000000, 1.088130];
 end
 
-[lin, col, pla] = size(Lab);
-if pla == 3
-  lin2 = lin * col;
-  Lab = reshape(Lab, lin2, pla);
-end
-
-Lab = Lab';
-
-%===============================================
-% Check sizes and allocate
-[~, n] = size(Lab);
-
-% Get Y from Lstar
-Y = LxxToY(Lab, whiteXYZ);
-
-% Compute F[1] = f(Y/Yn) from Y
-% Because subroutine is a vector routine, we put in and get/
-% back dummy values for X,Z
-fakeXYZ = [Y; Y; Y];
-F = XYZToF(fakeXYZ, whiteXYZ);
-
-% Compute ratio[0], ratio[2] from a*,b*, and ratio[1] */
-F(1, :) = (Lab(2,:) / 500.0) + F(2, :);
-F(3, :) = F(2, :) - (Lab(3, :) / 200.0);
-ratio = FToRatio(F);
-
-% Compute XYZ from the ratios
-XYZ = ratio .* (whiteXYZ * ones(1, n));
-XYZ = XYZ';
-if pla == 3
-  XYZ = reshape(XYZ, lin, col, pla);
-end
-
-end
-
-function ratio = FToRatio(F)
-% ratio = FToRatio(F)
-%
-% This is related to Lab calculations.
-%
-% 10/10/93    dhb   Converted from CAP C code.
-
-% Find sizes and allocate
-[m, n] = size(F);
-ratio = zeros(m, n);
-
-% Compute according to the range
-hIndex = find( F > 0.206893);
-lIndex = find( F <= 0.206893);
-if (~isempty(hIndex))
-  ratio(hIndex) = F(hIndex) .^ 3.0;
-end
-if (~isempty(lIndex))
-  ratio(lIndex) = (F(lIndex) - (16.0 / 116.0) * ones(length(lIndex), 1)) / 7.787;
-end
-
-end
-
-function F = XYZToF(XYZ,white)
-% F = XYZToF(XYZ,white)
-%
-% Compute the F function ratio from XYZ.
-% This is used in Lab calculations.
-%
-% 10/10/93    dhb   Converted from CAP C code.
-
-% Find size of data
-[m,n] = size(XYZ);
-F = zeros(m,n);
-
-% Compute the ratios w.r.t. white point
-ratio = XYZ ./ (white*ones(1,n));
-
-% Compute F from ratio
-lIndex = find(ratio <= 0.008856);
-hIndex = find(ratio > 0.008856);
-if (~isempty(hIndex))
-  F(hIndex) = ratio(hIndex).^(1.0/3.0);
-end
-if (~isempty(lIndex))
-  F(lIndex) = 7.787*ratio(lIndex) + (16.0/116.0)*ones(length(lIndex),1);
-end
+xyz = applycform(lab, makecform('lab2xyz', 'WhitePoint', WhiteReference));
 
 end
