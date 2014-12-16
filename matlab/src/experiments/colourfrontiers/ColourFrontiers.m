ea9987e7b3775560814dafa525587bbf7a911a8c
Arash Akbarinia <akbarinia.arash@gmail.com>
2014-12-16 14:04:16 +0100

    added new frontiers

--- matlab/src/experiments/colourfrontiers/ColourFrontiers.m
+++ matlab/src/experiments/colourfrontiers/ColourFrontiers.m
@@ -2,17 +2,28 @@ function FrontierTable = ColourFrontiers()
 %ColourFrontiers Summary of this function goes here
 %   Detailed explanation goes here
 
-% TODO: add other borders here
-
 FrontierTable = ...
   {
   %lum     colour1      colour2
+  %   '25',    'Green',     'Blue';
+  %   '25',    'Blue',      'Purple';
+  %   '25',    'Purple',    'Red';
+  '25',    'Red',       'Brown';
+  '25',    'Brown',     'Green';
+  
   '36',    'Green',     'Blue';
   '36',    'Blue',      'Purple';
   '36',    'Purple',    'Pink';
   '36',    'Pink',      'Red';
   '36',    'Red',       'Brown';
   '36',    'Brown',     'Green';
+  
+  %   '47',    'Green',     'Blue';
+  %   '47',    'Blue',      'Purple';
+  %   '47',    'Purple',    'Pink';
+  %   '47',    'Pink',      'Red';
+  '47',    'Red',       'Brown';
+  '47',    'Brown',     'Green';
   
   '58',    'Green',     'Blue';
   '58',    'Blue',      'Purple';
@@ -21,6 +32,13 @@ FrontierTable = ...
   '58',    'Red',       'Orange';
   '58',    'Orange',    'Yellow';
   '58',    'Yellow',    'Green';
+  
+  %   '70',    'Green',     'Blue';
+  %   '70',    'Blue',      'Purple';
+  %   '70',    'Purple',    'Pink';
+  %   '70',    'Pink',      'Orange';
+  '70',    'Orange',    'Yellow';
+  '70',    'Yellow',    'Green';
   
   '81',    'Green',     'Blue';
   '81',    'Blue',      'Purple';
--- matlab/src/experiments/colourfrontiers/FocalColours.m
+++ matlab/src/experiments/colourfrontiers/FocalColours.m
@@ -4,148 +4,223 @@ function [CartFocals, PolarFocals] = FocalColours()
 
 % TODO: add other luminances here
 
+CartFocals = struct();
+
 %% black
-namblack = ...
+
+CartFocals.black = ...
   [
-  36  -0.01   0.02;  %  Black  B&K
-  36  -2.12  -3.93;  %  Black  B&O
-  36  -4.60  -1.63;  %  Black  B&V
-  36  -2.00   0.00;  %  Black  B&V
-  36   0.00   0.00;  %  Black  B&V
-  36  -0.02   0.02;  %  Black  S&W
+  0,  0,  0;
   ];
 
 %% blue
-namblue = ...
+
+blue58 = ...
   [
-  58  -3.410  -48.08;  %  Blue  B&K
-  58  -9.610  -18.44;  %  Blue  B&O
-  58  -16.44  -30.00;  %  Blue  B&V
-  36  -5.000  -30.00;  %  Blue  B&V
-  81  -20.00  -20.00;  %  Blue  B&V
-  58  -12.92  -27.22;  %  Blue  S&W
+  58  -3.410  -48.08;  % B&K
+  58  -9.610  -18.44;  % B&O
+  58  -16.44  -30.00;  % B&V
+  58  -12.92  -27.22;  % S&W
   ];
 
-%% brown
-nambrown = ...
+CartFocals.blue = ...
   [
-  36  18.18  30.27;  %  Brown  B&K
-  36  14.81  21.60;  %  Brown  B&O
-  36  20.00  40.00;  %  Brown  B&V
-  36  23.33  50.00;  %  Brown  B&V
-  36  25.00  45.00;  %  Brown  B&V
-  36  13.07  33.61;  %  Brown  S&W
+  25,  -2,   -32;
+  36,  -5,   -30;  % B&V
+  47,  -8,   -30;
+  mean(blue58);
+  70,  -15,  -25;
+  81,  -20,  -20;  % B&V
+  ];
+
+
+%% brown
+
+brown36 = ...
+  [
+  36  18.18  30.27;  % B&K
+  36  14.81  21.60;  % B&O
+  36  20.00  40.00;  % B&V
+  36  23.33  50.00;  % B&V
+  36  25.00  45.00;  % B&V
+  36  13.07  33.61;  % S&W
+  ];
+
+CartFocals.brown = ...
+  [
+  25,  14,  50;
+  mean(brown36);
+  47,  24,  30;
   ];
 
 %% green
-namgreen = ...
+
+green58 = ...
   [
-  58  -52.69  9.700;  %  Green  B&K
-  58  -22.60  24.65;  %  Green  B&O
-  58  -21.41  14.72;  %  Green  B&V
-  36  -38.09  22.89;  %  Green  B&V
-  81  -20.00  20.00;  %  Green  B&V
-  58  -38.09  22.89;  %  Green  S&W
+  58  -52.69  9.700;  % B&K
+  58  -22.60  24.65;  % B&O
+  58  -21.41  14.72;  % B&V
+  58  -38.09  22.89;  % S&W
+  ];
+
+CartFocals.green = ...
+  [
+  25,  -40.00,  23.00;
+  36,  -38.09,  22.89;  % B&V
+  47,  -36.50,  21.00;
+  mean(green58);
+  70,  -27.00,  20.50;
+  81,  -20.00,  20.00;  % B&V
   ];
 
 %% grey
-namgrey = ...
+
+CartFocals.grey = ...
   [
-  58  -0.03  0.04;  %  Grey  B&K
-  58  -1.23  1.94;  %  Grey  B&O
-  58  -3.95  1.42;  %  Grey  B&V
-  58   2.00  2.00;  %  Grey  B&V
-  58   0.00  0.00;  %  Grey  B&V
-  58  -0.04  0.04;  %  Grey  S&W
+  25,  0,  0;
+  36,  0,  0;
+  47,  0,  0;
+  58,  0,  0;
+  70,  0,  0;
+  81,  0,  0;
   ];
 
 %% orange
-namorange = ...
+
+orange58 = ...
   [
-  58  44.69  79.79;  %  Orange  B&K
-  58  33.30  43.67;  %  Orange  B&O
-  58  25.00  35.00;  %  Orange  B&V
-  81  33.30  45.00;  %  Orange  B&V
-  81  35.00  50.00;  %  Orange  B&V
-  58  31.50  57.28;  %  Orange  S&W
+  58  44.69  79.79;  % B&K
+  58  33.30  43.67;  % B&O
+  58  25.00  35.00;  % B&V
+  58  31.50  57.28;  % S&W
+  ];
+
+orange81 = ...
+  [
+  81  33.30  45.00;  % B&V
+  81  35.00  50.00;  % B&V
+  ];
+
+CartFocals.orange = ...
+  [
+  47, 33, 56;
+  mean(orange58);
+  70, 34, 50;
+  mean(orange81);
   ];
 
 %% pink
-nampink = ...
+
+pink36 = ...
   [
-  36  43  -7;  %  Pink  B&V
-  36  45  -8;  %  Pink  B&V
-  58  30  -3;  %  Pink  B&V
-  58  42  -5;  %  Pink  B&V
-  81  37   7;  %  Pink  B&V
-  81  50  10;  %  Pink  B&V
+  36  43  -7;  % B&V
+  36  45  -8;  % B&V
+  ];
+
+pink58 = ...
+  [
+  58  30  -3;  % B&V
+  58  42  -5;  % B&V
+  ];
+
+pink81 = ...
+  [
+  81  37   7;  % B&V
+  81  50  10;  % B&V
+  ];
+
+CartFocals.pink = ...
+  [
+  mean(pink36);
+  47, 44, -5;
+  mean(pink58);
+  70, 45, -6;
+  mean(pink81);
   ];
 
 %% purple
-nampurple = ...
+
+purple36 = ...
   [
-  36  23  -40;  %  Purple  B&V
-  36  20  -33;  %  Purple  B&V
-  58  20  -30;  %  Purple  B&V
-  58  30  -40;  %  Purple  B&V
-  81  20  -25;  %  Purple  B&V
-  81  25  -27;  %  Purple  B&V
+  36  23  -40;  % B&V
+  36  20  -33;  % B&V
+  ];
+
+purple58 = ...
+  [
+  58  20  -30;  % B&V
+  58  30  -40;  % B&V
+  ];
+
+purple81 = ...
+  [
+  81  20  -25;  % B&V
+  81  25  -27;  % B&V
+  ];
+
+CartFocals.purple = ...
+  [
+  25, 22.0, -37.0;
+  mean(purple36);
+  47, 23.0, -35.5;
+  mean(purple58);
+  70, 23.5, -31.0;
+  mean(purple81);
   ];
 
 %% red
-namred = ...
+
+red36 = ...
   [
-  36  59.51  30.17;  %  Red  B&K
-  36  40.58  15.46;  %  Red  B&O
-  36  45.00  15.00;  %  Red  B&V
-  58  44.54  21.50;  %  Red  B&V
-  58  49.53  22.00;  %  Red  B&V
-  36  49.53  28.41;  %  Red  S&W
+  36  59.51  30.17;  % B&K
+  36  40.58  15.46;  % B&O
+  36  45.00  15.00;  % B&V
+  36  49.53  28.41;  % S&W
+  ];
+
+red58 = ...
+  [
+  58  44.54  21.50;  % B&V
+  58  49.53  22.00;  % B&V
+  ];
+
+CartFocals.red = ...
+  [
+  25, 47, 23;
+  mean(red36);
+  47, 48, 22;
+  mean(red58);
   ];
 
 %% white
-namwhite = ...
+
+CartFocals.white = ...
   [
-  81  -0.05  0.060;  %  White  B&K
-  81  -0.96  10.62;  %  White  B&O
-  81  -3.00  4.000;  %  White  B&V
-  81   3.00  0.000;  %  White  B&V
-  81   0.00  1.000;  %  White  B&V
-  81  -0.05  0.060;  %  White  S&W
+  100,  0,  0;
   ];
 
 %% yellow
-namyellow = ...
+
+yellow81 = ...
   [
   81  7.28  109.12;  %  Yellow  B&K
   81  3.35  56.360;  %  Yellow  B&O
   81  2.00  50.000;  %  Yellow  B&V
-  58  3.00  50.000;  %  Yellow  B&V
   81  0.00  46.000;  %  Yellow  B&V
   81  1.65  72.030;  %  Yellow  S&W
   ];
 
-%% putting cartesian in struct
-CartFocals = struct;
-CartFocals.black  = [mean(namblack);               mean(namblack);  [0, 0, 0]];
-CartFocals.blue   = [mean(namblue([1:4, 6], :));   mean(namblue);   mean(namblue([1:3, 5:6], :))];
-CartFocals.brown  = [mean(nambrown);               mean(nambrown);  [0, 0, 0]];
-CartFocals.green  = [mean(namgreen([1:4, 6], :));  mean(namgreen);  mean(namgreen([1:3, 5:6], :))];
-CartFocals.grey   = [mean(namgrey);                mean(namgrey);   mean(namgrey)];
-CartFocals.orange = [mean(namorange([1:3, 6], :)); mean(namorange); mean(namorange)];
-CartFocals.pink   = [mean(nampink(1:4, :));        mean(nampink);   mean(nampink(3:6, :))];
-CartFocals.purple = [mean(nampurple(1:4, :));      mean(nampurple); mean(nampurple(3:6, :))];
-CartFocals.red    = [mean(namred([1:3, 6], :));    mean(namred);    mean(namred(4:6, :))];
-CartFocals.white  = [[0, 0, 0];                    mean(namwhite);  mean(namwhite)];
-CartFocals.yellow = [namyellow(4, :);              mean(namyellow); mean(namyellow)];
-
-ColourNames = fieldnames(CartFocals);
-for i = 1:numel(ColourNames)
-  CartFocals.(ColourNames{i})(:, 1) = [36; 58; 81];
-end
+CartFocals.yellow = ...
+  [
+  58,  3.00,  50;  % B&V
+  70,  2.92,  58;
+  mean(yellow81);
+  ];
 
 %% converting them to polar
+
 PolarFocals = struct();
+ColourNames = fieldnames(CartFocals);
 for i = 1:numel(ColourNames)
   PolarFocals.(ColourNames{i}) = cart2pol3([CartFocals.(ColourNames{i})(:, 2:3), CartFocals.(ColourNames{i})(:, 1)]);
 end
