e921ad5d56abf3f7281636cb11b776c6d53f1b12
Arash Akbarinia <akbarinia.arash@gmail.com>
2014-12-17 11:13:44 +0100

    added new frontiers for grey borders.

--- matlab/src/experiments/colourfrontiers/GreyFrontiers.m
+++ matlab/src/experiments/colourfrontiers/GreyFrontiers.m
@@ -2,32 +2,39 @@ function FrontierTable = GreyFrontiers()
 %GreyFrontiers Summary of this function goes here
 %   Detailed explanation goes here
 
-% TODO: add other borders here
-
 FrontierTable = ...
   {
-  %lum     colour       w/contex        wo/context      angle limit
-  '36',    'Green',     3.619422689,    3.498391118,    'Brown';
-  '36',    'Blue',      4.934799618,    4.951908395,    'Green';
-  '36',    'Purple',    5.825916375,    5.764518826,    'Blue';
-  '36',    'Pink',      6.410524778,    6.428999015,    'Purple';
-  '36',    'Red',       0.629245673,    0.624122655,    'Pink';
-  '36',    'Brown',     1.814186927,    1.699024360,    'Red';
+  %lum     colour
+  '25',    'Red';
+  '25',    'Brown';
   
-  '58',    'Green',     3.497584341,    3.415861832,    'Yellow';
-  '58',    'Blue',      4.911725724,    4.947604147,    'Green';
-  '58',    'Purple',    5.745765196,    5.723958150,    'Blue';
-  '58',    'Pink',      6.487972025,    6.505966666,    'Purple';
-  '58',    'Red',       0.604454375,    0.604126142,    'Pink';
-  '58',    'Orange',    1.331458929,    1.258743082,    'Red';
-  '58',    'Yellow',    1.850395271,    1.842067225,    'Orange';
+  '36',    'Green';
+  '36',    'Blue';
+  '36',    'Purple';
+  '36',    'Pink';
+  '36',    'Red';
+  '36',    'Brown';
   
-  '81',    'Green',     3.426219373,    3.344970199,    'Yellow';
-  '81',    'Blue',      4.831389697,    4.901483523,    'Green';
-  '81',    'Purple',    5.655103896,    5.682502729,    'Blue';
-  '81',    'Pink',      0.516980892,    0.516793913,    'Purple';
-  '81',    'Orange',    1.304555370,    1.281320273,    'Pink';
-  '81',    'Yellow',    2.011381327,    2.029197077,    'Orange';
+  '47',    'Red';
+  '47',    'Brown';
+  
+  '58',    'Green';
+  '58',    'Blue';
+  '58',    'Purple';
+  '58',    'Pink';
+  '58',    'Red';
+  '58',    'Orange';
+  '58',    'Yellow';
+  
+  '70',    'Orange';
+  '70',    'Yellow';
+  
+  '81',    'Green';
+  '81',    'Blue';
+  '81',    'Purple';
+  '81',    'Pink';
+  '81',    'Orange';
+  '81',    'Yellow';
   };
 
 end
