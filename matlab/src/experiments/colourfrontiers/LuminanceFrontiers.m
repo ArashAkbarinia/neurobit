998209f6ef0b0ab0fb57bc1b971d28185a9d4007
Arash Akbarinia <akbarinia.arash@gmail.com>
2014-12-18 09:53:30 +0100

    new borders for luminance

--- matlab/src/experiments/colourfrontiers/LuminanceFrontiers.m
+++ matlab/src/experiments/colourfrontiers/LuminanceFrontiers.m
@@ -14,7 +14,10 @@ FrontierTable = ...
   %colour1    colour2       a       b       lum1     lum2
   'Black',     'Grey',      0.5,    0.5,    0.00,    0.50;
   'Grey',      'White',     0.5,    0.5,    0.50,    1.00;
-  %   'Red',       'Pink',      1.0,    0.5,    0.00,    0.50;
+  'Brown',     'Red',       1.0,    0.5,    0.00,    0.50;
+  'Red',       'Pink',      1.0,    0.5,    0.50,    1.00;
+  'Orange',    'Yellow',    0.5,    1.0,    0.50,    1.00;
+
   %   'Red',       'Orange',    1.0,    1.0,    0.50,    1.00;
   %   'Brown',     'Orange',    0.5,    1.0,    0.50,    1.00;
   %   'Brown',     'Yellow',    0.5,    1.0,    0.10,    1.00;
