1a911e64fe819740ac4f35d6c48f90dea6c302e4
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-01-05 16:07:49 +0100

    plotting all the llipsoids different input parameters

--- matlab/src/algorithms/colourcategorisation/PlotAllEllipsoids.m
+++ matlab/src/algorithms/colourcategorisation/PlotAllEllipsoids.m
@@ -1,12 +1,29 @@
-function [] = PlotAllEllipsoids(FilePath)
-%PlotAllEllipsoids Summary of this function goes here
-%   Detailed explanation goes here
+function [] = PlotAllEllipsoids(varargin)
+%PlotAllEllipsoids plotting all the ellipsoids from different inputs
+%
 
-MatFile = load(FilePath);
-ellipsoids = MatFile.ellipsoids;
-rgbs = MatFile.RGBValues;
+FigureNumber = 0;
 
-figure('NumberTitle', 'Off', 'Name', 'Colour Categorisation - Ellipsoids');
+if ischar(varargin{1})
+  MatFile = load(varargin{1});
+  ellipsoids = MatFile.ellipsoids;
+  rgbs = MatFile.RGBValues;
+  if nargin == 2
+    FigureNumber = varargin{2};
+  end
+else
+  ellipsoids = varargin{1};
+  rgbs = varargin{2};
+  if nargin == 3
+    FigureNumber = varargin{3};
+  end
+end
+
+if FigureNumber == 0
+  figure('NumberTitle', 'Off', 'Name', 'Colour Categorisation - Ellipsoids');
+else
+  figure(FigureNumber);
+end
 for i = 1:size(ellipsoids, 1)
   DrawEllipsoid(ellipsoids(i, :), 'FaceColor', [1, 1, 1], 'EdgeColor', rgbs(i, :), 'FaceAlpha', 0.3);
   hold on;
