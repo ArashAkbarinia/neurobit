e5a79f6b8e1dfdddc63fab7d1315c5c43af91992
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-01-07 09:26:25 +0100

    made the luminance frontiers a bit more felixable

--- matlab/src/experiments/colourfrontiers/LuminanceFrontiers.m
+++ matlab/src/experiments/colourfrontiers/LuminanceFrontiers.m
@@ -11,13 +11,13 @@ function FrontierTable = LuminanceFrontiers()
 
 FrontierTable = ...
   {
-  %colour1    colour2       a       b       lum1     lum2
+  %colour1     colour2      a       b       lum1     lum2
   'Black',     'Grey',      0.5,    0.5,    0.00,    0.50;
   'Grey',      'White',     0.5,    0.5,    0.50,    1.00;
   'Brown',     'Red',       1.0,    0.5,    0.00,    0.50;
-  'Red',       'Pink',      1.0,    0.5,    0.50,    1.00;
-  'Brown',     'Orange',    0.5,    1.0,    0.50,    0.75;
-  'Orange',    'Yellow',    0.5,    1.0,    0.75,    1.00;
+  'Red',       'Pink',      1.0,    0.5,    0.30,    1.00;
+  'Brown',     'Orange',    0.5,    1.0,    0.40,    0.80;
+  'Orange',    'Yellow',    0.5,    1.0,    0.70,    1.00;
   
   % maybe
   %   'Red',       'Orange',    1.0,    1.0,    0.50,    1.00;
