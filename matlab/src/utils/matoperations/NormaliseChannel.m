76af3d1612e65da7537a82963060c2e9973e6921
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-01-07 10:37:47 +0100

    normalising channels for data in rows or chans

--- matlab/src/utils/matoperations/NormaliseChannel.m
+++ matlab/src/utils/matoperations/NormaliseChannel.m
@@ -1,7 +1,7 @@
-function noarmalisedx = NormaliseChannel(x, a, b)
+function noarmalisedx = NormaliseChannel(x, a, b, mins, maxs)
 %NormaliseChannel change range of data between a and b.
 %   This function applies normalisation on each channel of the matrix. If
-%   'a' and 'b' are not provided the matrix 'x' is is scaled to brin all
+%   'a' and 'b' are not provided the matrix 'x' is is scaled to bring all
 %   values into the range [0, 1].
 %
 % Inputs
@@ -13,21 +13,45 @@ function noarmalisedx = NormaliseChannel(x, a, b)
 %   noarmalisedx the output matrix between 'a' and 'b'.
 %
 
-if nargin < 3
+if isempty(a)
   a = 0.0;
+end
+if isempty(b)
   b = 1.0;
 end
 
 [rows, cols, chns] = size(x);
+
+% TODO: make it generic for differnt channels.
+if chns ~= 3 && cols ~= 3
+  error('NormaliseChannel:wrongnumbers', 'NormaliseChannel only supports data in 3 channels.');
+end
+
+if chns == 1
+  x = reshape(x, rows, cols / 3, 3);
+end
+
 x = double(x);
 a = double(a);
 b = double(b);
 
-noarmalisedx = zeros(rows, cols, chns);
-for i = 1:chns
-  minv = min(min(x(:, :, i)));
-  maxv = max(max(x(:, :, i)));
+noarmalisedx = zeros(size(x));
+
+if isempty(mins)
+  mins = min(min(x));
+end
+if isempty(maxs)
+  maxs = max(max(x));
+end
+
+for i = 1:3
+  minv = mins(i);
+  maxv = maxs(i);
   noarmalisedx(:, :, i) = a + (x(:, :, i) - minv) * (b - a) / (maxv - minv);
+end
+
+if chns == 1
+  noarmalisedx = reshape(noarmalisedx, rows, cols);
 end
 
 end
