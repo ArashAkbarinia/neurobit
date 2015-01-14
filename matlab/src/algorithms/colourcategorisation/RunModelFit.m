712ce9372de285e9f37c0878f099840055e55f16
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-01-14 12:40:59 +0100

    merged colour ellipsoid fitting into RunModelFit

--- matlab/src/algorithms/colourcategorisation/RunModelFit.m
+++ matlab/src/algorithms/colourcategorisation/RunModelFit.m
@@ -5,7 +5,7 @@ if nargin < 1
 end
 if nargin < 2
   plotme = 1;
-  saveme = 1;
+  saveme = 0;
 end
 
 if strcmpi(WhichColours{1}, 'a')
@@ -280,5 +280,38 @@ view(-19, 54);
 grid on;
 set(gcf, 'color', [1, 1, 1]);
 hold off;
+
+end
+
+function RSS = ColourEllipsoidFitting(x, FittingData)
+% Describe the function
+% This function does the actual fitting of the three level datapoints
+% obtained by variables data36 data58 and data81 and the 3D-ellipsoid.
+% The datapoints correspond to the psychophysical results obtained through
+% colour categorization experiments in the frontiers between colour names.
+% The ellipsoid parameters are passed to the funtion though the variable x
+% which contains the centre and the semi-axes of the 3D-ellipsoid to fit to
+% the data. Imagine the ellipsoid as a rugby ball placed with its main axis
+% vertically. It intersects the three horizontal dataplanes (at three
+% luminance levels) forming three horizontal ellipses.
+% The actual fitting is done considering the distance between each
+% point in the luminance data plane and the closest point on the ellipse
+% for each plane. The output of this function is the residual
+% sum of squares (RSS) of these distances, added for the three planes.
+% Since the data in the lowest luminance plane has the largest variance, it
+% tends to bias the fitting. To counter this, we weighted the RSS
+% corresponding to each luminance plane.
+
+if ~isempty(FittingData.borders)
+  distances = DistanceEllipsoid(FittingData.borders, x, 0);
+  %  The Frobenius norm, sometimes also called the Euclidean norm (which may
+  %  cause confusion with the vector L^2-norm which also sometimes known as
+  %  the Euclidean norm), is matrix norm of an min matrix  A defined as the
+  %  square root of the sum of the absolute squares of its elements
+  %   RSS = norm(distances, 'fro');
+  RSS = mean(distances);
+else
+  RSS = 0;
+end
 
 end
