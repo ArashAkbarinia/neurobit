cda94c43e9b34243af632d5e5fe585cb1420ba09
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-01-19 15:30:09 +0100

    my own ground truth

--- matlab/src/experiments/wcs/WcsResults.m
+++ matlab/src/experiments/wcs/WcsResults.m
@@ -1,11 +1,30 @@
-function ChipsTable = WcsResults()
+function ChipsTable = WcsResults(sources)
 %WcsResults Summary of this function goes here
 %   Detailed explanation goes here
 
-ChipsTable1 = BerlinKayColourBoundries(true);
-ChipsTable2 = SturgesWhitfielColourBoundries();
-ChipsTable3 = BenaventeColourBoundries();
+if nargin < 1
+  sources = {'berlin', 'sturges', 'benavente', 'arash'};
+end
 
-ChipsTable = (ChipsTable1 + ChipsTable2 + ChipsTable3) / 3;
+ChipsTable = 0;
+
+sources = lower(sources);
+nsources = length(sources);
+for i = 1:nsources;
+  switch sources{i}
+    case {'berlin'}
+      ChipsTable = ChipsTable + BerlinKayColourBoundries(true);
+    case {'sturges'}
+      ChipsTable = ChipsTable + SturgesWhitfielColourBoundries();
+    case {'benavente'}
+      ChipsTable = ChipsTable + BenaventeColourBoundries();
+    case {'arash'}
+      ChipsTable = ChipsTable + ArashColourBoundries();
+    otherwise
+      disp(['Wrong category: ', sources{i}]);
+  end
+end
+
+ChipsTable = ChipsTable / nsources;
 
 end
