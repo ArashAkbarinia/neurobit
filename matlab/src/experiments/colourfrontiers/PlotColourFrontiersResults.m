bb0b77c9e0c81a28d8a5c0876e38b848bb8e3724
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-01-29 09:55:05 +0100

    plotting the experiment resutls with borders

--- matlab/src/experiments/colourfrontiers/PlotColourFrontiersResults.m
+++ matlab/src/experiments/colourfrontiers/PlotColourFrontiersResults.m
@@ -1,4 +1,4 @@
-function labs = PlotColourFrontiersResults(FilePath)
+function labs = PlotColourFrontiersResults(FilePath, condition)
 %PlotColourFrontiersResults Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -8,15 +8,43 @@ ExperimentResult = MatFile.ExperimentResults;
 angles = ExperimentResult.angles;
 radii = ExperimentResult.radii;
 luminances = ExperimentResult.luminances;
+conditions = ExperimentResult.conditions;
+ResultTable = [angles, radii, luminances, conditions'];
+ExperimentColours = lower(ExperimentResult.FrontierColours);
 
-nexperiments = length(angles);
+if nargin > 1
+  FilteredConditions = ResultTable(:, 4) == condition;
+  ResultTable = ResultTable(FilteredConditions, :);
+  ExperimentColours = ExperimentColours(FilteredConditions, :);
+end
+
+nexperiments = size(ResultTable, 1);
 labs = zeros(nexperiments, 3);
 for i = 1:nexperiments
-  labs(i, :) = pol2cart3([angles(i), radii(i), luminances(i)], 1);
+  labs(i, :) = pol2cart3([ResultTable(i, 1), ResultTable(i, 2), ResultTable(i, 3)], 1);
 end
 
 figure('NumberTitle', 'Off', 'Name', ['Colour Frontiers - ', ExperimentResult.type]);
-plot3(labs(:, 1), labs(:, 2), labs(:, 3), '*r');
+hold on;
 grid on;
+
+[CartFocals, ~] = FocalColours();
+[UniqueConditions, IndexConditions, ~] = unique(conditions);
+for i = UniqueConditions
+  ColourA = ExperimentColours{IndexConditions(i), 1};
+  colour1 = CartFocals.(ColourA);
+  ColourB = ExperimentColours{IndexConditions(i), 2};
+  colour2 = CartFocals.(ColourB);
+  
+  pp = [colour1(1, 1), colour1(1, 2)];
+  plot([pp(1), 0], [pp(2), 0], 'r');
+  text(pp(1), pp(2), ColourA, 'color', 'r');
+  
+  pp = [colour2(1, 1), colour2(1, 2)];
+  plot([pp(1), 0], [pp(2), 0], 'r');
+  text(pp(1), pp(2), ColourB, 'color', 'r');
+end
+
+plot(labs(:, 2), labs(:, 3), '*r');
 
 end
