efc0540ca1ca253c0656e51e81e0e14115060464
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-02-03 12:26:58 +0100

    if nan make it 0

--- matlab/src/evaluation/ContingencyTable.m
+++ matlab/src/evaluation/ContingencyTable.m
@@ -18,14 +18,26 @@ TN = length(TN(TN == 0));
 
 % sensitivity
 sens = TP / (TP + FN);
+if isnan(sens)
+  sens = 0;
+end
 
 % specificity
 spec = TN / (FP + TN);
+if isnan(spec)
+  spec = 0;
+end
 
 % positive predictive value
 ppv = TP / (TP + FP);
+if isnan(ppv)
+  ppv = 0;
+end
 
 % negative predictive value
 npv = TN / (FN + TN);
+if isnan(npv)
+  npv = 0;
+end
 
 end
