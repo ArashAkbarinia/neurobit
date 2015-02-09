f9ab6997d1bafa787f48b9d0ef1bfe3a502c7abe
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-02-09 16:14:51 +0100

    satfaces return either as image or column

--- matlab/src/imagefactory/SatfacesColourCube.m
+++ matlab/src/imagefactory/SatfacesColourCube.m
@@ -1,9 +1,15 @@
-function [WcsColourTable, GroundTruth] = SatfacesColourCube()
+function [WcsColourTable, GroundTruth] = SatfacesColourCube(OutputImage)
+
+if nargin < 1
+  OutputImage = false;
+end
 
 SatfacesMat = load('satfaces.mat');
 [WcsColourTable, GroundTruth] = ColourStruct2MatChans(SatfacesMat.ColourPoints);
 
-WcsColourTable = reshape(WcsColourTable, 512, 384, 3);
-GroundTruth = reshape(GroundTruth, 512, 384, 11);
+if OutputImage
+  WcsColourTable = reshape(WcsColourTable, 512, 384, 3);
+  GroundTruth = reshape(GroundTruth, 512, 384, 11);
+end
 
 end
