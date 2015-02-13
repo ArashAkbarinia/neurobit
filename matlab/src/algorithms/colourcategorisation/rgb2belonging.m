a658c9b82f634f8650ca20e4523c363e52ed17af
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-02-13 15:31:24 +0100

    cleaning up rgb2belonging

--- matlab/src/algorithms/colourcategorisation/rgb2belonging.m
+++ matlab/src/algorithms/colourcategorisation/rgb2belonging.m
@@ -1,6 +1,6 @@
 function BelongingImage = rgb2belonging(ImageRGB, ColourSpace, ConfigsMat, plotme, GroundTruth)
-%RGB2BELONGING  labels each pixel in the image as one of the focal
-%               eleven colours.
+%RGB2BELONGING  labels each pixel in the image as one of the focal eleven
+%               colours.
 
 if nargin < 2
   ColourSpace = 'lab';
@@ -11,44 +11,32 @@ if nargin < 5
   plotme = 0;
 end
 
-ColourConstantImage = ColourConstancyACE(ImageRGB);
-ColourConstantImage = uint8(NormaliseChannel(ColourConstantImage, 0, 255, [], []));
-
-% TODO: make a more permanent solution, this is just becuase 0 goes to the
-% end of the world
-ImageRGB = ImageRGB + 1;
-
+% TODO: try CMYK as well
 if strcmpi(ColourSpace, 'lsy')
   if isempty(ConfigsMat)
     ConfigsMat = load('lsy_ellipsoid_params_new');
   end
+  % TODO: make a more permanent solution, this is just becuase 0 goes to the
+  % end of the world (ImageRGB + 1)
   % gammacorrect = true, max pix value > 1, max luminance = daylight
-  ImageOpponent = XYZ2lsY(sRGB2XYZ(ImageRGB, true, [10 ^ 2, 10 ^ 2, 10 ^ 2]), 'evenly_ditributed_stds');
-  ImageOpponentConstant = XYZ2lsY(sRGB2XYZ(ColourConstantImage, true, [10 ^ 2, 10 ^ 2, 10 ^ 2]), 'evenly_ditributed_stds');
+  ImageOpponent = XYZ2lsY(sRGB2XYZ(ImageRGB + 1, true, [10 ^ 2, 10 ^ 2, 10 ^ 2]), 'evenly_ditributed_stds');
   axes = {'l', 's', 'y'};
 elseif strcmpi(ColourSpace, 'lab')
   if isempty(ConfigsMat)
     ConfigsMat = load('lab_ellipsoid_params_new');
   end
   ImageOpponent = double(applycform(ImageRGB, makecform('srgb2lab')));
-  ImageOpponentConstant = double(applycform(ColourConstantImage, makecform('srgb2lab')));
   axes = {'l', 'a', 'b'};
 end
 ColourEllipsoids = ConfigsMat.ColourEllipsoids;
 
-% BelongingImage(:, :, 1:8) = ChromaticEllipsoidBelonging(ImageOpponent, ColourEllipsoids);
 BelongingImage = AllEllipsoidsEvaluateBelonging(ImageOpponent, ColourEllipsoids);
-% BelongingImage(:, :, 9:11) = max(AchromaticEllipsoidBelonging(ImageOpponentConstant, ColourEllipsoids), BelongingImage(:, :, 9:11));
-BelongingImage(:, :, 9:11) = AchromaticEllipsoidBelonging(ImageOpponentConstant, ColourEllipsoids) * 0.33 + BelongingImage(:, :, 9:11) * 0.66;
-% BelongingImage(:, :, 9) = BelongingImage(:, :, 9) - max(BelongingImage(:, :, 1:8), [], 3);
-% BelongingImage(:, :, 10) = BelongingImage(:, :, 10) - max(BelongingImage(:, :, 1:8), [], 3);
-% BelongingImage(:, :, 11) = BelongingImage(:, :, 11) - max(BelongingImage(:, :, 1:8), [], 3);
 
 if plotme
   EllipsoidsTitles = ConfigsMat.RGBTitles;
   EllipsoidsRGBs = name2rgb(EllipsoidsTitles);
   % just for debugging purpose for the small images
-  PlotAllPixels(ImageRGB, ImageOpponent, ColourEllipsoids, EllipsoidsRGBs, axes, GroundTruth, -1);
+  PlotAllPixels(ImageRGB, ImageOpponent, ColourEllipsoids, EllipsoidsRGBs, axes, GroundTruth);
   
   PlotAllChannels(ImageRGB, BelongingImage, EllipsoidsTitles, EllipsoidsRGBs, 'Colour Categorisation - Colour Planes');
   if ~isempty(GroundTruth)
@@ -58,16 +46,17 @@ end
 
 end
 
-function [] = PlotAllPixels(ImageRGB, ImageOpponent, ColourEllipsoids, EllipsoidsRGBs, axes, GroundTruth, ColourIndex)
+function [] = PlotAllPixels(ImageRGB, ImageOpponent, ColourEllipsoids, EllipsoidsRGBs, axes, GroundTruth)
 
-if isempty(GroundTruth) || isempty(ColourIndex)
+if isempty(GroundTruth)
   return;
 end
 
 [rows, cols, chns] = size(ImageOpponent);
-if ColourIndex == -1
-  GroundTruth = ones(rows, cols, 1);
-  ColourIndex = 1;
+if rows * cols < 500
+  nfigures = 3;
+else
+  return;
 end
 
 ImageRGB = im2double(ImageRGB);
@@ -75,24 +64,12 @@ ImageRGB = reshape(ImageRGB, rows * cols, chns);
 ImageOpponent = reshape(ImageOpponent, rows * cols, chns);
 
 AxesViews = [0, 90; 0, 0; 90, 0;];
-if rows * cols < 500
-  nfigures = 3;
-else
-  nfigures = 1;
-end
 figure();
 for k = 1:nfigures
   h = subplot(1, nfigures, k);
   hold on;
   grid on;
   scatter3(ImageOpponent(:, 1), ImageOpponent(:, 2), ImageOpponent(:, 3), 36, ImageRGB);
-  %     for i = 1:size(ImageOpponent, 1)
-  %       for j = 1:size(ImageOpponent, 2)
-  %         if GroundTruth(i, j, ColourIndex) > 0
-  %           plot3(ImageOpponent(i, j, 1), ImageOpponent(i, j, 2), ImageOpponent(i, j, 3), 'marker', 'o', 'MarkerFaceColor', im2double(ImageRGB(i, j, :)), 'MarkerEdgeColor', [0, 0, 0]);
-  %         end
-  %       end
-  %     end
   PlotAllEllipsoids(ColourEllipsoids, EllipsoidsRGBs, h);
   xlabel(axes{1});
   ylabel(axes{2});
