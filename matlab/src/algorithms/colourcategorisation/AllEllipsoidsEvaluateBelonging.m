961b4a0dd984ebb670adfae02d33bd2e48b788da
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-02-13 16:50:32 +0100

    minor commit renaming

--- matlab/src/algorithms/colourcategorisation/AllEllipsoidsEvaluateBelonging.m
+++ matlab/src/algorithms/colourcategorisation/AllEllipsoidsEvaluateBelonging.m
@@ -20,11 +20,11 @@ belongings = zeros(rows, cols, nelpisd);
 if chns == 3
   % first, convert the picture into a giant vector where every row
   % correspond to a pixel
-  lsYVector = reshape(InputImage, rows * cols, chns);
+  InputVector = reshape(InputImage, rows * cols, chns);
 end
 
 for i = 1:nelpisd
-  [ibelonging, ~] = EllipsoidEvaluateBelonging(lsYVector, ColourEllipsoids(i, :));
+  [ibelonging, ~] = EllipsoidEvaluateBelonging(InputVector, ColourEllipsoids(i, :));
   
   belongings(:, :, i) = reshape(ibelonging, rows, cols, 1);
 end
