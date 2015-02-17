99560066b0ed8ee087b04ab94ba0f9d65674c6b6
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-02-17 11:47:42 +0100

    minor commit

--- matlab/src/experiments/wcs/WcsResults.m
+++ matlab/src/experiments/wcs/WcsResults.m
@@ -1,9 +1,15 @@
 function ChipsTable = WcsResults(sources)
-%WcsResults Summary of this function goes here
-%   Detailed explanation goes here
+%WcsResults  the colour naming results of different articles on Munsell.
+%
+% Inputs
+%   sources  the desired articles.
+%
+% Outputs
+%   ChipsTable  the probability map of each colour in 10x41x11 matrix.
+%
 
 if nargin < 1
-  sources = {'berlin', 'sturges', 'benavente', 'arash'};
+  sources = {'berlin', 'sturges', 'joost', 'robert', 'arash'};
 end
 
 ChipsTable = 0;
@@ -14,16 +20,16 @@ for i = 1:nsources;
   switch sources{i}
     case {'berlin'}
       TmpTable = BerlinKayColourBoundries(true);
+      TmpTable(TmpTable > 0) = 1;
       ArashTable = ArashColourBoundries();
       TmpTable([1, 10], 2:41, 9:11) = ArashTable([1, 10], 2:41, 9:11);
       ChipsTable = ChipsTable + TmpTable;
     case {'sturges'}
       TmpTable = SturgesWhitfielColourBoundries();
+      TmpTable(TmpTable > 0) = 1;
       ArashTable = ArashColourBoundries();
       TmpTable([1, 10], 2:41, 9:11) = ArashTable([1, 10], 2:41, 9:11);
       ChipsTable = ChipsTable + TmpTable;
-    case {'benavente'}
-      ChipsTable = ChipsTable + BenaventeColourBoundries();
     case {'joost'}
       ChipsTable = ChipsTable + ColourNamingTestImage(WcsChart(), 'joost', 0);
     case {'robert'}
