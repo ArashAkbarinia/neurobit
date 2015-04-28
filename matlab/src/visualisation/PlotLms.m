aa3ea07c3294274a5e1a691d82c0b3d925e5b1c2
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-04-28 16:41:38 +0200

    extracted plotting 2 channels from PlotLms

--- matlab/src/visualisation/PlotLms.m
+++ matlab/src/visualisation/PlotLms.m
@@ -8,30 +8,13 @@ function FigureNumber = PlotLms(InputImage)
 %   FigureNumber  the reference to the figure.
 %
 
-imlsm = rgb2lms(InputImage);
+imlms = rgb2lms(InputImage);
+FigureNumber = PlotRgb(imlms);
 
-l = imlsm(:, :, 1);
-m = imlsm(:, :, 2);
-s = imlsm(:, :, 3);
-
-lim = l;
-lim(:, :, 2:3) = 0;
-lim = uint8(lim);
-
-sim = s;
-sim(:, :, 3) = sim(:, :, 1);
-sim(:, :, 1:2) = 0;
-sim = uint8(sim);
-
-mim = m;
-mim(:, :, 2) = mim;
-mim(:, :, [1, 3]) = 0;
-mim = uint8(mim);
-
-FigureNumber = figure;
-subplot(2, 2, 1); imshow(InputImage);
-subplot(2, 2, 2); imshow(lim);
-subplot(2, 2, 3); imshow(sim);
-subplot(2, 2, 4); imshow(mim);
+figure(FigureNumber);
+subplot(2, 2, 1); imshow(InputImage); title('original');
+subplot(2, 2, 2); title('L Channel');
+subplot(2, 2, 3); title('M Channel');
+subplot(2, 2, 4); title('S Channel');
 
 end
