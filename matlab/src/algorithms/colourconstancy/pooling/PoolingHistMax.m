d12e1b1430065a5f9e11bebd99db2bfac370f065
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-05-08 14:53:40 +0200

    made the default value the maximum intensity

--- matlab/src/algorithms/colourconstancy/pooling/PoolingHistMax.m
+++ matlab/src/algorithms/colourconstancy/pooling/PoolingHistMax.m
@@ -6,16 +6,13 @@ function HistMax = PoolingHistMax(InputImage, nbins, CutoffPercent)
 %
 % inputs
 %   InputImage     the input image.
-%   nbins          number of discrete grey levels, default is max of type.
+%   nbins          number of discrete grey levels, default is max value.
 %   CutoffPercent  the cut off percentage, default is 0.01.
 %
 % outputs
 %   HistMax  the maximum value that satisfies CutoffPercent in range [0, 1]
 %
 
-if nargin < 2
-  nbins = 256;
-end
 if nargin < 3
   CutoffPercent = 0.01;
 end
@@ -28,14 +25,18 @@ maxnpizels = CutoffPercent * npixels;
 HistMax = zeros(1, 3);
 for i = 1:chns
   ichan = InputImage(:, :, i);
+  MaxVal = max(max(ichan));
+  if nargin < 2
+    nbins = ceil(MaxVal);
+  end
   ihist = imhist(ichan, nbins);
   
-  HistMax(1, i) = 1;
+  HistMax(1, i) = MaxVal;
   jpixels = 0;
   for j = nbins:-1:1
     jpixels = ihist(j) + jpixels;
     if jpixels >= maxnpizels
-      HistMax(1, i) = j ./ nbins;
+      HistMax(1, i) = j ./ nbins .* MaxVal;
       break;
     end
   end
