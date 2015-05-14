764f6efd9bc044cf82259435ddb64153f8e4545f
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-05-14 14:57:41 +0200

    make the inputs double

--- matlab/src/utils/matoperations/MatChansMulK.m
+++ matlab/src/utils/matoperations/MatChansMulK.m
@@ -1,16 +1,16 @@
 function matrixk = MatChansMulK(m, k)
-%MatChansMulK scalar multiplication in each channel.
+%MatChansMulK  scalar multiplication in each channel.
 %
 % inputs
 %   m  the input matrix
 %   k  the scalar vector contains same numebr of chanels as matrix.
-%
 %
 % outputs
 %   matrixk  the output matrix each chanel is multiplied by corresponding
 %            'k' scalar, i.e. matrixk(:, :, 1) =  m(:, :, 1) .* k(1)
 %
 
+m = double(m);
 [rows, cols, chns] = size(m);
 k = reshape(k, 1, 3);
 
