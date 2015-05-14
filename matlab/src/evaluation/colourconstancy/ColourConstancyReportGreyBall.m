c2b7eabd8d66a488996b89444fc9d29b6288e468
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-05-14 14:57:13 +0200

    returning the difference of luminance as well

--- matlab/src/evaluation/colourconstancy/ColourConstancyReportGreyBall.m
+++ matlab/src/evaluation/colourconstancy/ColourConstancyReportGreyBall.m
@@ -1,6 +1,18 @@
-function AngularErrors = ColourConstancyReportGreyBall(method, plotme, ImageNumbers)
-%COLOURCONSTANCYREPORTGREYBALL Summary of this function goes here
-%   Detailed explanation goes here
+function [AngularErrors, LuminanceDiffs] = ColourConstancyReportGreyBall(method, plotme, ImageNumbers)
+%ColourConstancyReportGreyBall  applies colour constancy on Grayball data.
+%   http://www.cs.sfu.ca/~colour/data/gray_ball/
+%
+% inputs
+%   method        name of the method to be used.
+%   plotme        if true the corrected images are displayed.
+%   ImageNumbers  number of images to be tested, by default all.
+%
+% outputs
+%   AngularErrors   the angular error between estimated luminance and
+%                   ground truth.
+%   LuminanceDiffs  the differences between estimated luminance and ground
+%                   truth.
+%
 
 if nargin < 1
   method = 'opponency';
@@ -10,9 +22,10 @@ if nargin < 2
 end
 
 FunctionPath = mfilename('fullpath');
-DataSetFolder = 'data/dataset/ColourConstancyDataSet/grayball/';
+DataSetFolder = 'data/dataset/ColourConstancyDataset/grayball/';
 DataSetMatPath = [DataSetFolder, 'GreyBallImageList.mat'];
 MatFilePath = strrep(FunctionPath, 'matlab/src/evaluation/colourconstancy/ColourConstancyReportGreyBall', DataSetMatPath);
+DataSetPath = strrep(FunctionPath, 'matlab/src/evaluation/colourconstancy/ColourConstancyReportGreyBall', DataSetFolder);
 GreyBallImageListMat = load(MatFilePath);
 
 GreyBallImageNames = GreyBallImageListMat.GreyBallImageNames;
@@ -24,9 +37,10 @@ if nargin < 3
 end
 
 AngularErrors = zeros(nimages, 1);
+LuminanceDiffs = zeros(nimages, 3);
 
 for i = ImageNumbers
-  CurrentImage = imread([DataSetFolder, GreyBallImageNames{i}]);
+  CurrentImage = imread([DataSetPath, GreyBallImageNames{i}]);
   
   if strcmpi(method, 'opponency')
     [~, EstimatedLuminance] = ColourConstancyOpponency(CurrentImage, false);
@@ -50,22 +64,30 @@ for i = ImageNumbers
   if plotme
     ColourConstantImage = MatChansMulK(CurrentImage, 1 ./ EstimatedLuminance);
     ColourConstantImage = NormaliseChannel(ColourConstantImage, [], [], [],[]);
-    
     ColourConstantImage = uint8(ColourConstantImage .* 255);
+    
+    GroundTruthImage = MatChansMulK(CurrentImage, 1 ./ GroundtruthLuminance);
+    GroundTruthImage = NormaliseChannel(GroundTruthImage, [], [], [],[]);
+    GroundTruthImage = uint8(GroundTruthImage .* 255);
+    
     figure;
     subplot(1, 3 , 1);
-    imshow(CurrentImage); title('original');
+    imshow(CurrentImage); title(['original ', num2str(i)]);
     subplot(1, 3 , 2);
-    imshow(ColourConstantImage); title('Colour constant estimated');
+    imshow(ColourConstantImage); title(['Colour constant estimated - angular error ', num2str(CurrentAngularError)]);
     subplot(1, 3 , 3);
-    imshow(ColourConstantImage); title('Colour constant groundtruth');
+    imshow(GroundTruthImage); title('Colour constant groundtruth');
   end
   
-  fprintf('%d - angular error %f\n', i, roundn(CurrentAngularError, -1));
+  fprintf('%d - angular error %f\n', i, CurrentAngularError);
   AngularErrors(i, :) = CurrentAngularError;
+  
+  CurrentLumDiff = (GroundtruthLuminance ./ max(GroundtruthLuminance)) - reshape(EstimatedLuminance ./ max(EstimatedLuminance), 1, 3);
+  LuminanceDiffs(i, :) = CurrentLumDiff;
 end
 
 
 fprintf('Average angular error %f\n', mean(AngularErrors));
+fprintf('Average luminance difference [%f %f %f]\n', mean(abs(LuminanceDiffs)));
 
 end
