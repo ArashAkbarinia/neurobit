b01280f063979bba3d6a8bca4f9251f202de7cfa
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-05-19 16:47:46 +0200

    fairly stable version of constancy through opponency

--- matlab/src/algorithms/colourconstancy/ColourConstancyOpponency.m
+++ matlab/src/algorithms/colourconstancy/ColourConstancyOpponency.m
@@ -1,4 +1,4 @@
-function ColourConstantImage = ColourConstancyOpponency(InputImage, plotme)
+function [ColourConstantImage, luminance] = ColourConstancyOpponency(InputImage, plotme)
 %ColourConstancyOpponency Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -11,46 +11,40 @@ if plotme
 end
 
 [rows, cols, chns] = size(InputImage);
-InputImage = im2double(InputImage);
+InputImageDouble = double(InputImage);
 
-rch = InputImage(:, :, 1);
-gch = InputImage(:, :, 2);
-bch = InputImage(:, :, 3);
-ych = (rch + gch) ./ 2;
-lch = rch + gch + bch;
-
+% EQUATION: eq-2.4-2.8 Ebner 2007, "Color Constancy"
 rgb2do = ...
   [
   1 / sqrt(2), -1 / sqrt(2),  0;
-  1 / sqrt(6),  1 / sqrt(6), -2 / sqrt(6);
+  1 / sqrt(6),  1 / sqrt(6), -sqrt(2 / 3);
   1 / sqrt(3),  1 / sqrt(3),  1 / sqrt(3);
   ];
-% do2rgb = inv(rgb2do);
-opponent = rgb2do * reshape(InputImage, rows * cols, chns)';
+do2rgb = rgb2do';
+
+opponent = rgb2do * reshape(InputImageDouble, rows * cols, chns)';
 opponent = reshape(opponent', rows, cols, chns);
 
+% cone opoonent retinal ganglion cells
 rg = opponent(:, :, 1);
 yb = opponent(:, :, 2);
 wb = opponent(:, :, 3);
-
-% rg = (rch - gch) ./ sqrt(2);
-% yb = (ych - 2 * bch) ./ sqrt(6);
-% wb = lch ./ sqrt(3);
 
 if plotme
   PlotLmsOpponency(InputImage);
 end
 
-sorg = SingleOpponent(rg, 1);
-soyb = SingleOpponent(yb, 1);
-sowb = SingleOpponent(wb, 1);
-
 lambda = 3;
+sorg = SingleOpponent(rg, lambda);
+soyb = SingleOpponent(yb, lambda);
+sowb = SingleOpponent(wb, lambda);
+
+lambda = lambda * 2;
 sogr = SingleOpponent(-rg, lambda);
 soby = SingleOpponent(-yb, lambda);
 sobw = SingleOpponent(-wb, lambda);
 
-k = 0.5;
+k = 0.9;
 dorg = DoubleOpponent(sorg, sogr, k);
 doyb = DoubleOpponent(soyb, soby, k);
 dowb = DoubleOpponent(sowb, sobw, k);
@@ -69,31 +63,31 @@ doresponse(:, :, 1) = dorg;
 doresponse(:, :, 2) = doyb;
 doresponse(:, :, 3) = dowb;
 doresponse = reshape(doresponse, rows * cols, chns);
-dtmap = (rgb2do \ doresponse')';
+dtmap = (do2rgb * doresponse')';
 dtmap = reshape(dtmap, rows, cols, chns);
 
-figure;
-subplot(1, 2 , 1);
-imshow(InputImage); title('original');
-subplot(1, 2 , 2);
-imshow(NormaliseChannel(dtmap, [], [], [], [])); title('dt map');
-% figure;imshow(NormaliseChannel(dtmap-InputImage, [], [], [], []))
+luminance = CalculateLuminance(dtmap);
+luminance = reshape(luminance, 1, 3);
+ColourConstantImage = MatChansMulK(InputImageDouble, 1 ./ luminance);
 
+if plotme
+  ColourConstantImage = NormaliseChannel(ColourConstantImage, [], [], [],[]);
+  
+  ColourConstantImage = uint8(ColourConstantImage .* 255);
+  figure;
+  subplot(1, 2 , 1);
+  imshow(InputImage); title('original');
+  subplot(1, 2 , 2);
+  imshow(ColourConstantImage); title('Colour constant');
+end
+
+end
+
+function luminance = CalculateLuminance(dtmap)
+
+% luminance = mean(mean(dtmap));
 MaxVals = max(max(dtmap));
-k = 1 / MaxVals;
-ColourConstantImage = MatChansMulK(dtmap, k);
-
-% MaxVals = sum(sum(max(dtmap, [], 3)));
-% ColourConstantImage = InputImage ./ MaxVals;
-
-ColourConstantImage = NormaliseChannel(ColourConstantImage, [], [], [],[]);
-
-ColourConstantImage = uint8(ColourConstantImage .* 255);
-figure;
-subplot(1, 2 , 1);
-imshow(InputImage); title('original');
-subplot(1, 2 , 2);
-imshow(ColourConstantImage); title('Colour constant');
+luminance = MaxVals;
 
 end
 
@@ -103,9 +97,40 @@ if nargin < 2
   lambda = 3;
 end
 
-rf = fspecial('gaussian', [3, 3], 0.5 * lambda);
+CentreSize = 5;
+SurroundSize = 11;
+hc = fspecial('average', CentreSize);
+MeanCentre = conv2(isignal, hc / CentreSize ^ 2, 'same');
+SigmaCentre = sqrt(conv2(isignal .^ 2, hc / CentreSize ^ 2, 'same') - MeanCentre .^ 2);
 
-rfresponse = imfilter(isignal, rf);
+hs = fspecial('average', SurroundSize);
+MeanSurround = conv2(isignal, hs / SurroundSize ^ 2, 'same');
+SigmaSurround = sqrt(conv2(isignal .^ 2, hs / SurroundSize ^ 2, 'same') - MeanSurround .^ 2);
+
+% EQUATION: eq-4 Otazu et al. 2007, "Multiresolution wavelet framework
+% models brightness induction effect"
+r    = SigmaCentre ./ (SigmaSurround + 1.e-6);
+zctr = r .^ 2 ./ (1 + r .^ 2);
+
+% nContrastLevels = 8;
+% levels = multithresh(zctr, nContrastLevels - 1);
+nContrastLevels = 4;
+LevelGap = 1 / nContrastLevels;
+levels = 0:LevelGap:1;
+ContrastLevels = imquantize(zctr, levels(2:end-1));
+SigmaIncrease = 1 / (nContrastLevels - 1);
+
+rfresponse = zeros(size(zctr));
+for i = nContrastLevels:-1:1
+  lambdax = lambda * ((nContrastLevels - i) * SigmaIncrease + 1);
+  lambday = lambda * 1.05 * ((nContrastLevels - i) * SigmaIncrease + 1);
+  rfi = GaussianFilter2(lambdax, lambday, 0, 0);
+  rfresponsei = imfilter(isignal, rfi);
+  rfresponse(ContrastLevels == i) = rfresponsei(ContrastLevels == i);
+end
+
+% rf = GaussianFilter2(lambda, lambda + 1, 0, 0);
+% rfresponse = imfilter(isignal, rf);
 
 end
 
@@ -115,27 +140,7 @@ if nargin < 3
   k = 0.5;
 end
 
-MidaMin = 4;
-nplans = floor(log(max(size(ab) - 1) / MidaMin) / log(2)) + 1;
-[wab, cab] = DWT(ab, nplans);
-[wba, cba] = DWT(ba, nplans);
-
-wp = cell(nplans, 1);
-wc = cell(nplans, 1);
-for s = 1:nplans
-  % for horizontal, vertical and diagonal orientations:
-  for orientation = 1:3
-    wsab = wab{s, 1}(:, :, orientation);
-    wsba = wba{s, 1}(:, :, orientation);
-    wp{s, 1}(:, :, orientation) = wsab + k * wsba;
-  end
-  csab = cab{s, 1};
-  csba = cba{s, 1};
-  wc{s, 1} = csab + k * csba;
-end
-
-[rows, cols] = size(ab);
-rfresponse = IDWT(wp, wc, cols, rows);
+rfresponse = ab + k .* ba;
 
 end
 
