cc120f2e2683b9cf9b0149e41740395e9fd8b90a
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-05-26 09:57:54 +0200

    support for list and number in frequencies and orientations

--- matlab/src/filterfactory/gabor/GaborBank.m
+++ matlab/src/filterfactory/gabor/GaborBank.m
@@ -1,9 +1,9 @@
-function KernelsBank = GaborBank(frequencies, orientations)
+function KernelsBank = GaborBank(frequencies, orientations, MinFrequency, MaxFrequency)
 %GaborBank generates a bank of Gabor kernels.
 %
 % inputs
-%   frequencies   desired frequencies.
-%   orientations  desired orientations. It can be a number or list.
+%   frequencies   desired frequencies, it can be a number or list.
+%   orientations  desired orientations, it can be a number or list.
 %
 % outputs
 %   KernelsBank   created kernels bank. Rows (x dimension) corresponds to
@@ -13,25 +13,54 @@ function KernelsBank = GaborBank(frequencies, orientations)
 % See also GaborKernel2
 %
 
-% TODO: support for both list and number for frequency and orientation.
+if nargin < 1 || isempty(frequencies)
+  frequencies = 5;
+end
+if nargin < 2 || isempty(orientations)
+  orientations = 4;
+end
 
-u = frequencies;
-v = orientations;
+if length(frequencies) == 1
+  u = frequencies;
+  
+  if nargin < 3 || isempty(MinFrequency)
+    MinFrequency = 0.05;
+  end
+  if nargin < 4 || isempty(MaxFrequency)
+    MaxFrequency = 0.45;
+  end
+  FrequencyIncreament = (MaxFrequency - MinFrequency) / u;
+  
+  frequencies = zeros(1, u);
+  for f = 1:u
+    frequencies(1, f) = FrequencyIncreament * (f - 1) + MinFrequency;
+  end
+else
+  u = length(frequencies);
+end
+
+if length(orientations) == 1
+  v = orientations;
+  
+  orientations = zeros(1, v);
+  for o = 1:v
+    orientations(1, o) = ((o - 1) / v) * pi;
+  end
+else
+  v = length(orientations);
+end
 
 KernelsBank = cell(u, v);
 
-MinFrequency = 0.05;
-MaxFrequency = 0.25;
-FrequencyIncreament = (MaxFrequency - MinFrequency) / u;
 bandwidth = 1;
 offset = 0;
 scale = 1;
 
 for f = 1:u
-  frequency = FrequencyIncreament * (f - 1) + MinFrequency;
+  frequency = frequencies(1, f);
   for o = 1:v
-    theta = ((o - 1) / v) * pi;
-    KernelsBank{f, o} = GaborKernel2(theta, [], [], bandwidth, frequency, offset, scale);
+    orientation = orientations(1, o);
+    KernelsBank{f, o} = GaborKernel2(orientation, [], [], bandwidth, frequency, offset, scale);
   end
 end
 
