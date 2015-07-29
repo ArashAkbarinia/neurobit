a663c5aabe21d042224c86eb7d7d3c6e92c75334
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-07-29 09:40:29 +0200

    separated the switch case from angulr error calculation

--- matlab/src/evaluation/colourconstancy/ColourConstancyReportAlgoithms.m
+++ matlab/src/evaluation/colourconstancy/ColourConstancyReportAlgoithms.m
@@ -1,5 +1,5 @@
 function [EstimatedLuminance, CurrentAngularError, CurrentLumDiff] = ColourConstancyReportAlgoithms(CurrentImage, method, GroundtruthLuminance, isxyz)
-%ColourConstancyReportAlgoithms  estimatied luminance of am algorithm.
+%ColourConstancyReportAlgoithms  estimatied luminance and calculate error.
 %
 % inputs
 %   CurrentImage          the image to be processed.
@@ -17,41 +17,7 @@ if nargin < 4 || isempty(isxyz)
   isxyz = false;
 end
 
-if strcmpi(method, 'nothing')
-  EstimatedLuminance = [1, 1, 1];
-elseif strcmpi(method, 'grey world')
-  [~, EstimatedLuminance] = ColourConstancyGreyWorld(CurrentImage);
-elseif strcmpi(method, 'hist white patch')
-  [~, EstimatedLuminance] = ColourConstancyHistWhitePatch(CurrentImage);
-elseif strcmpi(method, 'hist white patch local std')
-  [~, EstimatedLuminance] = ColourConstancyHistWhitePatch(CurrentImage, -1);
-elseif strcmpi(method, 'white patch')
-  [~, EstimatedLuminance] = ColourConstancyWhitePatch(CurrentImage);
-elseif strcmpi(method, 'local std')
-  [~, EstimatedLuminance] = ColourConstancyLocalStd(CurrentImage);
-elseif strcmpi(method, 'colour edge')
-  [~, EstimatedLuminance] = ColourConstancyColourEdge(CurrentImage);
-elseif strcmpi(method, 'gao')
-  EstimatedLuminance = GaoDOCC_demo(CurrentImage);
-elseif strcmpi(method, 'joost')
-  EstimatedLuminance = JoostColorConstancyDemo(CurrentImage);
-elseif strcmpi(method, 'gamut mapping')
-  EstimatedLuminance = GamutMappingColourConstancy(CurrentImage);
-elseif strcmpi(method, 'bayesian')
-  EstimatedLuminance = BayesianColourConstancy(CurrentImage);
-else
-  [~, EstimatedLuminance] = ColourConstancyOpponency(CurrentImage, false, method);
-end
-if isxyz
-  EstimatedLuminance = EstimatedLuminance ./ max(EstimatedLuminance(:));
-  EstimatedLuminance = applycform(EstimatedLuminance, makecform('xyz2srgb'));
-end
-
-EstimatedLuminance = EstimatedLuminance';
-
-% normalising the illuminant
-EstimatedNorm = sum(EstimatedLuminance(:));
-EstimatedLuminance = EstimatedLuminance ./ EstimatedNorm;
+EstimatedLuminance = ColourConstancySwitchAlgorithms(CurrentImage, method, isxyz);
 
 % normalising the groundtruth
 GroundtruthNorm = sum(GroundtruthLuminance(:));
---
+++ matlab/src/evaluation/colourconstancy/ColourConstancySwitchAlgorithms.m
@@ -0,0 +1,53 @@
+function EstimatedLuminance = ColourConstancySwitchAlgorithms(CurrentImage, method, isxyz)
+%ColourConstancySwitchAlgorithms  estimatied luminance of an algorithm.
+%
+% inputs
+%   CurrentImage          the image to be processed.
+%   method                desired method.
+%   isxyz                 if the image is in XYZ space, default false.
+%
+% outputs
+%   EstimatedLuminance    estimated luminance of the selected method.
+%
+
+if nargin < 3 || isempty(isxyz)
+  isxyz = false;
+end
+
+if strcmpi(method, 'nothing')
+  EstimatedLuminance = [1, 1, 1];
+elseif strcmpi(method, 'grey world')
+  [~, EstimatedLuminance] = ColourConstancyGreyWorld(CurrentImage);
+elseif strcmpi(method, 'hist white patch')
+  [~, EstimatedLuminance] = ColourConstancyHistWhitePatch(CurrentImage);
+elseif strcmpi(method, 'hist white patch local std')
+  [~, EstimatedLuminance] = ColourConstancyHistWhitePatch(CurrentImage, -1);
+elseif strcmpi(method, 'white patch')
+  [~, EstimatedLuminance] = ColourConstancyWhitePatch(CurrentImage);
+elseif strcmpi(method, 'local std')
+  [~, EstimatedLuminance] = ColourConstancyLocalStd(CurrentImage);
+elseif strcmpi(method, 'colour edge')
+  [~, EstimatedLuminance] = ColourConstancyColourEdge(CurrentImage);
+elseif strcmpi(method, 'gao')
+  EstimatedLuminance = GaoDOCC_demo(CurrentImage);
+elseif strcmpi(method, 'joost')
+  EstimatedLuminance = JoostColorConstancyDemo(CurrentImage);
+elseif strcmpi(method, 'gamut mapping')
+  EstimatedLuminance = GamutMappingColourConstancy(CurrentImage);
+elseif strcmpi(method, 'bayesian')
+  EstimatedLuminance = BayesianColourConstancy(CurrentImage);
+else
+  [~, EstimatedLuminance] = ColourConstancyOpponency(CurrentImage, false, method);
+end
+if isxyz
+  EstimatedLuminance = EstimatedLuminance ./ max(EstimatedLuminance(:));
+  EstimatedLuminance = applycform(EstimatedLuminance, makecform('xyz2srgb'));
+end
+
+EstimatedLuminance = EstimatedLuminance';
+
+% normalising the illuminant
+EstimatedNorm = sum(EstimatedLuminance(:));
+EstimatedLuminance = EstimatedLuminance ./ EstimatedNorm;
+
+end
