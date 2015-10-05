99b549e979737ce8576fbdfb54cb14ca0d7612c4
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-10-05 14:10:55 +0200

    added rotation angle

--- matlab/src/filterfactory/gaussian/GaussianFilter2.m
+++ matlab/src/filterfactory/gaussian/GaussianFilter2.m
@@ -1,4 +1,4 @@
-function h = GaussianFilter2(sigmax, sigmay, meanx, meany)
+function h = GaussianFilter2(sigmax, sigmay, meanx, meany, theta)
 %GaussianFilter2  Gaussian kernel.
 %   Explanation  http://mathworld.wolfram.com/GaussianFunction.html
 %
@@ -7,6 +7,7 @@ function h = GaussianFilter2(sigmax, sigmay, meanx, meany)
 %   sigmay  the sigma in y direction, default 0.5.
 %   meanx   the mean in x direction, default centre.
 %   meany   the mean in y direction, default centre.
+%   theta   the rotation angle.
 %
 % outputs
 %   h  the Gaussian kernel.
@@ -29,6 +30,10 @@ if nargin < 4 || isempty(meany)
   meany = 0;
 end
 
+if nargin < 5 || isempty(theta)
+  theta = 0;
+end
+
 centrex = (sizex + 1) / 2;
 centrey = (sizey + 1) / 2;
 centrex = centrex + (meanx * centrex);
@@ -37,7 +42,11 @@ centrey = centrey + (meany * centrey);
 xs = linspace(1, sizex, sizex)' * ones(1, sizey) - centrex;
 ys = ones(1, sizex)' * linspace(1, sizey, sizey) - centrey;
 
-h = exp(-0.5 .* ((xs ./ sigmax) .^ 2 + (ys ./ sigmay) .^ 2)) ./ (2 * pi * sigmax * sigmay);
+a =  cos(theta) ^ 2 / 2 / sigmax ^ 2 + sin(theta) ^ 2 / 2 / sigmay ^ 2;
+b = -sin(2 * theta) / 4 / sigmax ^ 2 + sin(2 * theta) / 4 / sigmay ^ 2;
+c =  sin(theta) ^ 2 / 2 / sigmax ^ 2 + cos(theta) ^ 2 / 2 / sigmay ^ 2;
+
+h = exp(-(a * xs .^ 2 + 2 * b * xs .* ys + c * ys .^ 2));
 
 h = h ./ sum(h(:));
 
