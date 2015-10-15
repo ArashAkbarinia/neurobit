918b43810e3860dff74082ba755802e7e412058b
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-10-15 17:53:23 +0200

    minor commit

--- matlab/src/evaluation/boundary/bench_bsds500.m
+++ matlab/src/evaluation/boundary/bench_bsds500.m
@@ -1,35 +1,40 @@
 % addpath benchmarks
 
 %clear all;close all;clc;
-
+TestName = 'arash';
 FOLDERPATH = '/home/arash/Software/Repositories/neurobit/data/dataset/BSR/';
 
 ImageDirectory = [FOLDERPATH, 'BSDS500/data/images/test'];
-ResultDirectory = [FOLDERPATH, 'BSDS500/results/dog'];
+ResultDirectory = [FOLDERPATH, 'BSDS500/results/', TestName];
 mkdir(ResultDirectory);
 
 ImageList = dir([ImageDirectory, '/*.jpg']);
 nfiles = length(ImageList);
 tic;
-for i = 1:nfiles
+parfor i = 1:nfiles
   disp(['processing ', ImageList(i).name]);
   CurrentFileName = ImageList(i).name;
   CurrentImage = imread([ImageDirectory, '/', CurrentFileName]);
+  CurrentImage = double(CurrentImage) ./ 255;
   
-%   ConfigsMat = load('/home/arash/Software/Repositories/neurobit/lab_ellipsoid_params_new');
-%   ConfigsMat.ColourEllipsoids(:, 10) = 0.01;
-%   BelongingImage = rgb2belonging(CurrentImage, ConfigsMat, false, []);
-  
-  CurrentImage = double(rgb2gray(CurrentImage));
-  CurrentImage = CurrentImage ./ 255;
-  EdgeImage = SurroundModulationEdgeDetector(CurrentImage);
-%   EdgeImage = zeros(size(BelongingImage));
-%   for j = 1:11
-%     EdgeImage(:, :, j) = pbCanny(BelongingImage(:, :, j));
-%   end
-%   EdgeImage = pbCannyColour(CurrentImage);
-%   EdgeImage = sum(EdgeImage, 3);
-  EdgeImage = uint8(NormaliseChannel(EdgeImage, 0, 255, [], []));
+% %   ConfigsMat = load('/home/arash/Software/Repositories/neurobit/lab_ellipsoid_params_new');
+% %   ConfigsMat.ColourEllipsoids(:, 10) = 0.01;
+% %   BelongingImage = rgb2belonging(CurrentImage, ConfigsMat, false, []);
+%   
+%   CurrentImage = double(rgb2gray(CurrentImage));
+%   CurrentImage = CurrentImage ./ 255;
+%   EdgeImage = SurroundModulationEdgeDetector(CurrentImage);
+% %   EdgeImage = zeros(size(BelongingImage));
+% %   for j = 1:11
+% %     EdgeImage(:, :, j) = pbCanny(BelongingImage(:, :, j));
+% %   end
+% %   EdgeImage = pbCannyColour(CurrentImage);
+% %   EdgeImage = sum(EdgeImage, 3);
+%   EdgeImage = uint8(NormaliseChannel(EdgeImage, 0, 255, [], []));
+
+  EdgeImage = SCOBoundary(CurrentImage, 0.5);
+%   EdgeImage = SurroundModulationEdgeDetector(CurrentImage);
+
   ResultName = CurrentFileName(1:end-4);
   imwrite(EdgeImage, [ResultDirectory, '/', ResultName, '.png']);
 end
@@ -40,8 +45,8 @@ toc;
 FOLDERPATH = '/home/arash/Software/Repositories/neurobit/data/dataset/BSR/';
 
 GroundtruthDirectory = [FOLDERPATH, 'BSDS500/data/groundTruth/test'];
-PlotsDirectory = [FOLDERPATH, 'BSDS500/plots/dog'];
-ResultDirectory = [FOLDERPATH, 'BSDS500/results/dog'];
+PlotsDirectory = [FOLDERPATH, 'BSDS500/plots/', TestName];
+ResultDirectory = [FOLDERPATH, 'BSDS500/results/', TestName];
 ImageDirectory = [FOLDERPATH, 'BSDS500/data/images/test'];
 mkdir(PlotsDirectory);
 nthresh = 99;
@@ -51,4 +56,3 @@ boundaryBench(ImageDirectory, GroundtruthDirectory, ResultDirectory, PlotsDirect
 toc;
 
 plot_eval(PlotsDirectory);
-
