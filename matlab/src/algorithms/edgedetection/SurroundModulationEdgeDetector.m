4b6cf28beed8058e8042e245fcd4fb309ab8f5bf
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-12-10 09:31:03 +0100

    v1.0 beat result of yang

--- matlab/src/algorithms/edgedetection/SurroundModulationEdgeDetector.m
+++ matlab/src/algorithms/edgedetection/SurroundModulationEdgeDetector.m
@@ -18,6 +18,7 @@ if size(InputImage, 3) == 3
   OpponentImage = double(applycform(uint8(InputImage .* 255), makecform('srgb2lab'))) ./ 255;
   OpponentImage(:, :, 4) = InputImage(:, :, 1) - 0.7 .* InputImage(:, :, 2);
   OpponentImage(:, :, 5) = InputImage(:, :, 3) - 0.7 .* mean(InputImage(:, :, 2:3), 3);
+  OpponentImage(:, :, 6) = LocalStdContrast(rgb2gray(InputImage));
 else
   OpponentImage = InputImage;
 end
