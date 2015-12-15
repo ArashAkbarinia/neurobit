5c35aebc7df3ed776e80cb7fdd1b9b3b0f81b9ea
Arash Akbarinia <akbarinia.arash@gmail.com>
2015-12-15 15:33:37 +0100

    parfor instead of for

--- matlab/src/algorithms/colourcategorisation/belonging/AllEllipsoidsEvaluateBelonging.m
+++ matlab/src/algorithms/colourcategorisation/belonging/AllEllipsoidsEvaluateBelonging.m
@@ -26,7 +26,7 @@ end
 
 belongings = zeros(VectorRows, nelpisd);
 
-for i = 1:nelpisd
+parfor i = 1:nelpisd
   [ibelonging, ~] = EllipsoidEvaluateBelonging(InputImage, ColourEllipsoids(i, :));
   
   belongings(:, i) = ibelonging;
