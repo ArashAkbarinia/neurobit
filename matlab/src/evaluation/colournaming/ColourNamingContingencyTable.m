344169f97fc9b20a4e15a66ed7787feb255f5409
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-01-07 14:54:20 +0100

    printing accuracy, f1 and error rate

--- matlab/src/evaluation/colournaming/ColourNamingContingencyTable.m
+++ matlab/src/evaluation/colournaming/ColourNamingContingencyTable.m
@@ -1,14 +1,19 @@
-function [] = ColourNamingContingencyTable(DirPath, method)
+function [] = ColourNamingContingencyTable(DirPath, method, GtIndex)
 
-if nargin < 2
+if nargin < 3
   DirPath = '/home/arash/Software/Repositories/neurobit/data/dataset/ColourNameDataset/ebay/';
   method = 'ourlab';
+  GtIndex = 2;
 end
 
 method = lower(method);
 disp(['Reading results for method ', method]);
 
 SubFolders = GetSubFolders(DirPath);
+if GtIndex == 1
+  indeces = cellfun('isempty', strfind(SubFolders(:, 1), 'FruitsVegetablesFlowers'));
+  SubFolders = SubFolders(indeces, :);
+end
 
 nSubFolder = length(SubFolders);
 ErrorMatsAll = zeros(nSubFolder, 8);
@@ -22,60 +27,48 @@ for j = 1:nSubFolder
   for k = 1:nSubSubFolder
     DirPathJK = [DirPathJ, SubSubFolders{k}, '/'];
     ResultDirectory = [DirPathJK, method, '_results/'];
-    FolderResultMat = load([ResultDirectory, 'ErrorMats.mat']);
+    FolderResultMat = load([ResultDirectory, 'ErrorMats', num2str(GtIndex), '.mat']);
     nimages = length(FolderResultMat.ErrorMats);
     CurrentErrorMat = zeros(nimages, 8);
-    for l = 1:nimages
-      CurrentErrorMat(l, :) = struct2array(FolderResultMat.ErrorMats{l});
+    if strcmpi(SubFolders{j}, 'FruitsVegetablesFlowers')
+      for l = 1:nimages
+        CurrentErrorMat(l, :) = struct2array(FolderResultMat.ErrorMats{l});
+      end
+      ErrorMatsCat(k, :) = mean(CurrentErrorMat, 1);
+    else
+      for l = 11:nimages
+        CurrentErrorMat(l, :) = struct2array(FolderResultMat.ErrorMats{l});
+      end
+      ErrorMatsCat(k, :) = mean(CurrentErrorMat(11:nimages, :), 1);
     end
-    ErrorMatsCat(k, :) = mean(CurrentErrorMat, 1);
   end
   ErrorMatsAll(j, :) = mean(ErrorMatsCat, 1);
-  fprintf('%s\t- Sensitivity: %0.2f Specificity %0.2f Positive predictive %0.2f Negative predictive %0.2f\n', SubFolders{j}, ErrorMatsAll(j, 1:4));
-  SaveResults(DirPathJ, method, ErrorMatsCat);
+  [acc, f1, er] = ReportResults(ErrorMatsAll(j, :));
+  fprintf('%s\t- Accuracy %0.2f F1-Score %0.2f Error-Rate %0.2f \n', SubFolders{j}, acc, f1, er);
+  SaveResults(DirPathJ, method, ErrorMatsCat, GtIndex);
 end
-SaveResults(DirPath, method, ErrorMatsAll);
+SaveResults(DirPath, method, ErrorMatsAll, GtIndex);
 
-fprintf('All average\t- Sensitivity: %0.2f Specificity %0.2f Positive predictive %0.2f Negative predictive %0.2f\n', mean(ErrorMatsAll(:, 1:4), 1));
-
-PrintPixelAverage(ErrorMatsAll);
-
-end
-
-function [] = PrintPixelAverage(ErrorMatsAll)
-
-sa = sum(ErrorMatsAll, 1);
-tp = sa(1, 5);
-fp = sa(1, 6);
-fn = sa(1, 7);
-tn = sa(1, 8);
-
-sens = tp / (tp + fn);
-if isnan(sens)
-  sens = 0;
-end
-% specificity
-spec = tn / (fp + tn);
-if isnan(spec)
-  spec = 0;
-end
-% positive predictive value
-ppv = tp / (tp + fp);
-if isnan(ppv)
-  ppv = 0;
-end
-% negative predictive value
-npv = tn / (fn + tn);
-if isnan(npv)
-  npv = 0;
-end
-
-fprintf('Pixel average\t- Sensitivity: %0.2f Specificity %0.2f Positive predictive %0.2f Negative predictive %0.2f\n', sens, spec, ppv, npv);
+[acc, f1, er] = ReportResults(sum(ErrorMatsAll, 1));
+fprintf('Sum: Accuracy %0.2f F1-Score %0.2f Error-Rate %0.2f\n', acc, f1, er);
 
 end
 
-function [] = SaveResults(DirPathJ, method, ErrorMats) %#ok
+function [acc, f1, er] = ReportResults(ErrorMat)
 
-save([DirPathJ, method, 'ErrorMats.mat'], 'ErrorMats');
+tp = ErrorMat(1, 5);
+fp = ErrorMat(1, 6);
+fn = ErrorMat(1, 7);
+tn = ErrorMat(1, 8);
+
+acc = (tp + tn) / (tp + fp + fn + tn);
+f1 = (2 * tp) / ((2 * tp) + fp + fn);
+er = fn / (tp + fn);
+
+end
+
+function [] = SaveResults(DirPathJ, method, ErrorMats, GtIndex) %#ok
+
+save([DirPathJ, method, 'ErrorMats', num2str(GtIndex), '.mat'], 'ErrorMats');
 
 end
