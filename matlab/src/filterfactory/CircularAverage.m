70360ae5b8d91cdaf7e913e04e1ac57ca5e21db9
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-02-25 11:04:43 +0100

    minor commit

--- matlab/src/filterfactory/CircularAverage.m
+++ matlab/src/filterfactory/CircularAverage.m
@@ -8,7 +8,10 @@ function ch = CircularAverage(radius)
 %   ch  the circle average filter
 %
 
-x = radius * 2 + 1;
+x = radius * 2;
+if mod(x, 2) == 0
+  x = x + 1;
+end
 [rr, cc] = meshgrid(1:x);
 centre = ceil(x / 2);
 ch = sqrt((rr - centre) .^ 2 + (cc - centre) .^ 2) <= radius;
--- matlab/src/utils/matoperations/CentreCircularZero.m
+++ matlab/src/utils/matoperations/CentreCircularZero.m
@@ -16,6 +16,9 @@ if CentreRadius == 0 || CentreRadius > min(ws, hs)
 end
 
 x = max(ws, hs);
+if mod(x, 2) == 0
+  x = x + 1;
+end
 [rr, cc] = meshgrid(1:x);
 centre = ceil(x / 2);
 ch = sqrt((rr - centre) .^ 2 + (cc - centre) .^ 2) <= CentreRadius;
