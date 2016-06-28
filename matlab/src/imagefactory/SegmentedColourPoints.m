e2444701a5760ccd9bd0f9e28e6ec793480bad46
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-06-28 10:43:34 +0200

    added support for choosing the index of ground truth, also for contrast variabality

--- matlab/src/imagefactory/SegmentedColourPoints.m
+++ matlab/src/imagefactory/SegmentedColourPoints.m
@@ -1,4 +1,4 @@
-function [ColourBoxesImage, GroundTruthImage] = SegmentedColourPoints(DirPath, nLimistPoitns)
+function [ColourBoxesImage, GroundTruthImage] = SegmentedColourPoints(DirPath, nLimistPoitns, ContrastDependant, GtIndex)
 
 if nargin < 1
   DirPath = '/home/arash/Software/Repositories/neurobit/data/dataset/ColourNameDataset/ebay/';
@@ -7,6 +7,13 @@ end
 if isempty(strfind(DirPath, '.mat'))
   if nargin < 2
     nLimistPoitns = 1000;
+  end
+  if nargin < 3
+    ContrastDependant = false;
+  end
+  if nargin < 4
+    % the *2.png are the rectified ground truths.
+    GtIndex = 2;
   end
   ColourPoints = EmptyColourPointsStruct();
   SubFolders = GetSubFolders(DirPath);
@@ -20,7 +27,7 @@ if isempty(strfind(DirPath, '.mat'))
       ColourName = lower(SubSubFolders{k});
       
       ImageFiles = dir([DirPathJK, '*.jpg']);
-      MaskFiles = dir([DirPathJK, '*.png']);
+      MaskFiles = dir([DirPathJK, ['*', num2str(GtIndex), '.png']]);
       nimages = length(ImageFiles);
       if nimages ~= length(MaskFiles)
         warning(['Directory ', DirPathJK, ' does not have same number of pictures and gts.']);
@@ -29,6 +36,16 @@ if isempty(strfind(DirPath, '.mat'))
       for i = 1:nimages
         ImagePath = [DirPathJK, ImageFiles(i).name];
         ImageRGB = imread(ImagePath);
+        
+        % if it's contrast dependant we first convolve the image with a
+        % gaussian kernel of different size based on the contrast of the
+        % pixel.
+        if ContrastDependant
+          ImageRGB = double(ImageRGB) ./ 255;
+          rfresponse = ContrastDependantGaussian(ImageRGB, 1.5);
+          ImageRGB = uint8(rfresponse .* 255);
+        end
+        
         [rows, cols, ~] = size(ImageRGB);
         ImageRGB = reshape(ImageRGB, rows * cols, 3);
         
