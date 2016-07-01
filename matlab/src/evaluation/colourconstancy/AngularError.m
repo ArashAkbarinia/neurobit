7ebbb08c213b1ba360c4e26367d0a66c2b942879
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-07-01 17:05:39 +0200

    documented recovery angular error

--- matlab/src/evaluation/colourconstancy/AngularError.m
+++ matlab/src/evaluation/colourconstancy/AngularError.m
@@ -1,10 +1,20 @@
 function r = AngularError(l1, l2)
+%AngularError  calculated the recover angular error.
+%
+% inputs
+%   l1  the estimated illuminant.
+%   l2  the ground truth illuminant.
+%
+% outputs
+%   r   the recovery angular error.
+%
 
 l1 = l1 ./ sum(l1(:));
 l2 = l2 ./ sum(l2(:));
 
 l1 = l1 / sqrt(sum(l1 .^ 2));
 l2 = l2 / sqrt(sum(l2 .^ 2));
+
 r = acosd(sum(l1(:) .* l2(:)));
 
 end
