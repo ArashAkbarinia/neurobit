0b93aac1a4c9fb278b54a330d5f007f3aeea76b6
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-07-01 17:16:56 +0200

    support for column based errors

--- matlab/src/evaluation/colourconstancy/TrimeanError.m
+++ matlab/src/evaluation/colourconstancy/TrimeanError.m
@@ -9,13 +9,18 @@ function OutputTrimean = TrimeanError(AngularErrors)
 %   TrimeanError  the Trimean value.
 %
 
-AngularErrors = sort(AngularErrors);
+cols = size(AngularErrors, 2);
+OutputTrimean = zeros(1, cols);
 
-l = floor(length(AngularErrors) / 4);
-
-m1 = AngularErrors(l);
-m2 = AngularErrors(l * 2);
-m3 = AngularErrors(l * 3);
-OutputTrimean = (0.25 * m1) + (0.5 * m2) + (0.25 * m3);
+for i = 1:cols
+  AngularErrorsColI = sort(AngularErrors(:, i));
+  
+  l = floor(length(AngularErrorsColI) / 4);
+  
+  m1 = AngularErrorsColI(l);
+  m2 = AngularErrorsColI(l * 2);
+  m3 = AngularErrorsColI(l * 3);
+  OutputTrimean(1, i) = (0.25 * m1) + (0.5 * m2) + (0.25 * m3);
+end
 
 end
