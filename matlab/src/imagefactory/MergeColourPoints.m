e9c1ed9bc93e0a34e16f8c4b38b1c4e78e2188b0
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-07-05 12:07:25 +0200

    a function to merge the segmented points of different datasets

---
+++ matlab/src/imagefactory/MergeColourPoints.m
@@ -0,0 +1,41 @@
+function [WcsColourTable, GroundTruth] = MergeColourPoints()
+%MERGECOLOURPOINTS Summary of this function goes here
+%   Detailed explanation goes here
+
+MatPaths = ...
+  {
+  '/home/arash/Software/Repositories/neurobit/matlab/data/mats/colourcategorisation/noconstancy/EbayPixelCounts.mat';
+  '/home/arash/Software/Repositories/neurobit/matlab/data/mats/colourcategorisation/noconstancy/YuanliuCarPixelPoints.mat';
+  '/home/arash/Software/Repositories/neurobit/matlab/data/mats/colourcategorisation/noconstancy/YuanliuMCNPixelPoints.mat';
+  '/home/arash/Software/Repositories/neurobit/matlab/data/mats/colourcategorisation/noconstancy/YuanliuSmallObjectsPixelPoints.mat';
+  };
+
+rgbs = [];
+gts = [];
+for i = 1:numel(MatPaths)
+  CurrentMat = load(MatPaths{i});
+  rgbs = [rgbs; CurrentMat.ColourBoxesImage]; %#ok
+  TmpGts = CurrentMat.GroundTruthImage;
+  TmpGts = reshape(TmpGts, size(TmpGts, 1), size(TmpGts, 3));
+  gts = [gts; TmpGts]; %#ok
+end
+
+[WcsColourTable, GroundTruth] = UnifiedRgbs(rgbs, gts);
+GroundTruth = reshape(GroundTruth, size(GroundTruth, 1), 1, size(GroundTruth, 2));
+GroundTruthCount = sum(GroundTruth, 3);
+for i = 1:size(WcsColourTable, 1)
+  GroundTruth(i, 1, :) = GroundTruth(i, 1, :) ./ GroundTruthCount(i);
+end
+
+end
+
+function [rgbs, gts] = UnifiedRgbs(ColourBoxesImage, GroundTruthImage)
+
+[rgbs, ~, IndUniqes] = unique(ColourBoxesImage, 'rows');
+
+gts = zeros(size(rgbs, 1), 11);
+for i = 1: 11
+  gts(:, i) = accumarray(IndUniqes, GroundTruthImage(:, i));
+end
+
+end
