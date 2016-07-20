663c955e198c29527a4980ce8bbf5304caea52e1
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-07-20 14:41:15 +0200

    gamma correction

---
+++ matlab/src/transformations/colourspaces/GammaCorrection.m
@@ -0,0 +1,22 @@
+function OutputImage = GammaCorrection(InputImage, GammaValue)
+%GammaCorrection  correcting the image with the given gamma.
+%   https://en.wikipedia.org/wiki/Gamma_correction
+%
+% inputs
+%   InputImage  the input image.
+%   GammaValue  the gamma value.
+%
+% outputs
+%   OutputImage  the gamma corrected image.
+%
+
+if nargin < 2
+  GammaValue = 1;
+elseif nargin ==2 && GammaValue < 0
+  GammaValue = 1;
+end
+
+InputImage = double(InputImage);
+OutputImage = uint8(255 * (InputImage ./ 255) .^ GammaValue);
+
+end
