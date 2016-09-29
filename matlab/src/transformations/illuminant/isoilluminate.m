9fce240498ed929754aa8efec8ecc140e88c1247
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-09-29 12:00:28 +0200

    isoillinator

---
+++ matlab/src/transformations/illuminant/isoilluminate.m
@@ -0,0 +1,16 @@
+function IsoilluminatedImage = isoilluminate(InputImage)
+%ISOILLUMINATE  convert a chromatic image into an isoilluminant one.
+%
+% inputs
+%   InputImage  the input chromatic image.
+%
+% outputs
+%   IsoilluminatedImage  the coloured isoilluminated output image.
+%
+
+InputImge = double(InputImage);
+MeanImage = sum(InputImge, 3);
+
+IsoilluminatedImage = InputImge ./ repmat(MeanImage, [1, 1, 3]);
+
+end
