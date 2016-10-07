4991c9fad1d9d54c2a95219d36852e46ef5d6cdb
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-10-07 14:25:09 +0200

    minor commit, figure titles

--- matlab/src/visualisation/PlotRgb.m
+++ matlab/src/visualisation/PlotRgb.m
@@ -8,16 +8,19 @@ function FigureNumber = PlotRgb(InputImage)
 %   FigureNumber  the reference to the figure.
 %
 
+FigureNumber = figure('name', 'RGB Channels');
+subplot(2, 2, 1); imshow(InputImage); title('original');
+
+InputImage = double(InputImage);
+
 rch = InputImage(:, :, 1);
 gch = InputImage(:, :, 2);
 bch = InputImage(:, :, 3);
 
-lim = ColouredImageRed(rch);
+lim = ColouredImageRedGreen(rch);
 mim = ColouredImageGreen(gch);
 sim = ColouredImageBlue(bch);
 
-FigureNumber = figure;
-subplot(2, 2, 1); imshow(InputImage); title('original');
 subplot(2, 2, 2); imshow(lim); title('R Channel');
 subplot(2, 2, 3); imshow(sim); title('B Channel');
 subplot(2, 2, 4); imshow(mim); title('G Channel');
