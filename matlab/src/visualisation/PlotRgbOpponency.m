23d0c70be7631b27100a456c20dba81b05e3aca7
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-10-07 14:41:33 +0200

    minor commit

--- matlab/src/visualisation/PlotRgbOpponency.m
+++ matlab/src/visualisation/PlotRgbOpponency.m
@@ -8,18 +8,14 @@ function FigureNumber = PlotRgbOpponency(InputImage)
 %   FigureNumber  the reference to the figure.
 %
 
-rgim(:, :, 1) =  InputImage(:, :, 1) - InputImage(:, :, 2);
-rgim(:, :, 2) = -InputImage(:, :, 1) + InputImage(:, :, 2);
-rgim(:, :, 3) = 0;
-rgim = NormaliseChannel(rgim, [], [], [], []);
-
-ybim(:, :, 1) =  InputImage(:, :, 1) + InputImage(:, :, 2) - InputImage(:, :, 3);
-ybim(:, :, 2) =  InputImage(:, :, 1) + InputImage(:, :, 2) - InputImage(:, :, 3);
-ybim(:, :, 3) = -InputImage(:, :, 1) - InputImage(:, :, 2) + InputImage(:, :, 3);
-ybim = NormaliseChannel(ybim, [], [], [], []);
-
-FigureNumber = figure;
+FigureNumber = figure('name', 'RGB Opponency');
 subplot(2, 2, 1:2); imshow(InputImage); title('original');
+
+InputImage = double(InputImage);
+
+rgim = ColouredImageRedGreen(InputImage(:, :, 1) - InputImage(:, :, 2));
+ybim = ColouredImageYellowBlue(0.5 .* InputImage(:, :, 1) + 0.5 .* InputImage(:, :, 2) - InputImage(:, :, 3));
+
 subplot(2, 2, 3); imshow(rgim); title('R-G Opponency');
 subplot(2, 2, 4); imshow(ybim); title('Y-B Opponency');
 
