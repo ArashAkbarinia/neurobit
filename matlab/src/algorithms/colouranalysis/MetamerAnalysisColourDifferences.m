50d6c8f734c3ad6635baad2c04aff5534b6e156c
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-11-02 16:32:17 +0100

    minor commit, considering all diferent deltaEs

--- matlab/src/algorithms/colouranalysis/MetamerAnalysisColourDifferences.m
+++ matlab/src/algorithms/colouranalysis/MetamerAnalysisColourDifferences.m
@@ -19,10 +19,20 @@ MetamerReport.CompMat1994 = CompMat1994;
 MetamerReport.CompMat2000 = CompMat2000;
 
 threshold = 0.5;
+MetamerReport.metamers1976 = CompMat1976 < threshold;
+MetamerReport.metamers1976(logical(eye(size(MetamerReport.metamers1976)))) = 0;
+MetamerReport.metamers1994 = CompMat1994 < threshold;
+MetamerReport.metamers1994(logical(eye(size(MetamerReport.metamers1994)))) = 0;
+MetamerReport.metamers2000 = CompMat2000 < threshold;
+MetamerReport.metamers2000(logical(eye(size(MetamerReport.metamers2000)))) = 0;
 MetamerReport.metamers = CompMat1976 < threshold & CompMat1994 < threshold & CompMat2000 < threshold;
 MetamerReport.metamers(logical(eye(size(MetamerReport.metamers)))) = 0;
 
-nAll = sum(MetamerReport.metamers(:)) / 2;
-disp(['Metamer percentage: ', num2str(nAll / ((nSignals * (nSignals - 1)) / 2))]);
+nAll = sum(MetamerReport.metamers1976(:)) / 2;
+disp(['Metamer-1976 percentage: ', num2str(nAll / ((nSignals * (nSignals - 1)) / 2))]);
+nAll = sum(MetamerReport.metamers1994(:)) / 2;
+disp(['Metamer-1994 percentage: ', num2str(nAll / ((nSignals * (nSignals - 1)) / 2))]);
+nAll = sum(MetamerReport.metamers2000(:)) / 2;
+disp(['Metamer-2000 percentage: ', num2str(nAll / ((nSignals * (nSignals - 1)) / 2))]);
 
 end
