130127c78bafca2b5d96c56b57033afddd93321c
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-11-08 10:15:38 +0100

    support for rgb images in MCI

--- matlab/src/evaluation/boundary/other/MCIcontour/MCIContour.m
+++ matlab/src/evaluation/boundary/other/MCIcontour/MCIContour.m
@@ -1,7 +1,7 @@
 % demo:
 % paper in IEEE Trans. Image Processing, 2014:
 % Kai-Fu Yang, Chao-Yi Li, and Yong-Jie Li*.
-% Multifeature-based Surround Inhibition Improves Contour Detection in 
+% Multifeature-based Surround Inhibition Improves Contour Detection in
 % Natural Images. IEEE TIP, 2014.
 %
 % Contact:
@@ -18,30 +18,34 @@
 
 function Pb = MCIContour(map)
 
-% set parameters...
-angles = 8;
-sigmas = [2.0 4.0];
-alpha =3.0; 
-p = 0.9;  % the percent of edge Pixels used in hysteresis thresholding
+for i = 1:size(map, 3)
+  % set parameters...
+  angles = 8;
+  sigmas = [2.0 4.0];
+  alpha =3.0;
+  p = 0.9;  % the percent of edge Pixels used in hysteresis thresholding
+  
+  % read original image (only for gray image)
+  % map = imread('302008.jpg'); % 302008.jpg  elephant_2.pgm
+  % figure;imshow(map,[]);
+  
+  % Contour Detector
+  [Res theta] = MCIoperator(map(:, :, i),sigmas,alpha,angles);
+  
+  % non-maxima suppression and hysteresis thresholding
+  theta = (theta-1)*pi/angles;
+  [Pb(:, :, i) bm] = NonmaxHysthresh(Res,theta, p);
+  % figure;imshow(1-Pb,[]); % Show soft contour map after non-maxima suppression
+  % figure;imshow(1-bm,[]); % Show binary contour map after hysteresis thresholding
+  
+  % Evaluation with P-measure...
+  % only for RuG40 dataset
+  % [P eFN eFP] = PMeasure(im,bm);  % im -- groundtruth map
+  
+  fprintf(2,'======== THE END ========\n');
+end
 
-% read original image (only for gray image)
-% map = imread('302008.jpg'); % 302008.jpg  elephant_2.pgm
-% figure;imshow(map,[]); 
-
-% Contour Detector
-[Res theta] = MCIoperator(map,sigmas,alpha,angles);
-
-% non-maxima suppression and hysteresis thresholding
-theta = (theta-1)*pi/angles;
-[Pb bm] = NonmaxHysthresh(Res,theta, p); 
-% figure;imshow(1-Pb,[]); % Show soft contour map after non-maxima suppression
-% figure;imshow(1-bm,[]); % Show binary contour map after hysteresis thresholding
-
-% Evaluation with P-measure...  
-% only for RuG40 dataset
-% [P eFN eFP] = PMeasure(im,bm);  % im -- groundtruth map
-
-fprintf(2,'======== THE END ========\n');
+Pb = mean(Pb, 3);
 %=========================================================================%
 
 end
