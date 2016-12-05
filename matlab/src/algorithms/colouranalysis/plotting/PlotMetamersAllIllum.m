ca13bb353f266cc9ae2c7337e61d637c0ecd21df
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-12-05 11:19:24 +0100

    saving the illustration stuff

--- matlab/src/algorithms/colouranalysis/plotting/PlotMetamersAllIllum.m
+++ matlab/src/algorithms/colouranalysis/plotting/PlotMetamersAllIllum.m
@@ -52,10 +52,24 @@ end
 
 PlotInd = 1;
 black = reshape([0, 0, 0], 1, 1, 3);
+
+if ~isempty(ResultDirectory)
+  % this is the array that can be saved for visulation proposes
+  illus.rows = zeros(nTops, 1);
+  illus.cols = zeros(nTops, 1);
+  illus.signal1 = cell(nTops, 1);
+  illus.signal2 = cell(nTops, 1);
+  illus.wavelength1 = cell(nTops, 1);
+  illus.wavelength2 = cell(nTops, 1);
+  illus.rgbrows = zeros(nTops, c - 1, 3);
+  illus.rgbcols = zeros(nTops, c - 1, 3);
+end
+
 for i = 1:nTops
   [row, col] = find(MetamersDis == UniqueDistances(i));
   row = row(1);
   col = col(1);
+  
   set(0, 'CurrentFigure', FigureHandler.m);
   subplot(r, c, PlotInd);
   PlotInd = PlotInd + 1;
@@ -75,6 +89,16 @@ for i = 1:nTops
   signal1 = reshape(signal1, size(signal1, 3), 1);
   signal2 = reshape(signal2, size(signal2, 3), 1);
   
+  if ~isempty(ResultDirectory)
+    % illus stuff
+    illus.rows(i) = row;
+    illus.cols(i) = col;
+    illus.signal1{i, :} = signal1;
+    illus.signal2{i, :} = signal2;
+    illus.wavelength1{i, :} = wavelength1;
+    illus.wavelength2{i, :} = wavelength2;
+  end
+  
   plot(wavelength1, signal1 ./ sum(signal1), 'color', rand(1,3));
   plot(wavelength2, signal2 ./ sum(signal2), 'color', rand(1,3));
   xlim([min(wavelength1(1), wavelength2(1)), max(wavelength1(end), wavelength2(end))]);
@@ -86,6 +110,10 @@ for i = 1:nTops
     subplot(r, c, PlotInd);
     PlotInd = PlotInd + 1;
     image([rgb(row, j - 1, :), rgb(row, j - 1, :), black, rgb(col, j - 1, :), rgb(col, j - 1, :)]);
+    if ~isempty(ResultDirectory)
+      illus.rgbrows(i, j - 1, :) = rgb(row, j - 1, :);
+      illus.rgbcols(i, j - 1, :) = rgb(col, j - 1, :);
+    end
     if i == 1
       title(labels{j - 1});
     end
@@ -96,6 +124,7 @@ end
 if ~isempty(ResultDirectory)
   saveas(FigureHandler.m, [ResultDirectory, '/MetamerSignals-', name, '.jpg']);
   close(FigureHandler.m);
+  save([ResultDirectory, '/MetamerSignals-', name, '.mat'], 'illus');
 end
 
 end
