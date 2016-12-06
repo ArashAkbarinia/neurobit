6219e3aca6ca9ff13854735847e00ea95fc2c463
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-12-06 14:48:51 +0100

    removed defult params

--- matlab/src/algorithms/colouranalysis/reporting/ReportColourNamingResults.m
+++ matlab/src/algorithms/colouranalysis/reporting/ReportColourNamingResults.m
@@ -2,11 +2,6 @@ function MetamerColourNameReport = ReportColourNamingResults(MetamerPairs, Curre
 %ReportColourNamingResults Summary of this function goes here
 %   Detailed explanation goes here
 
-if nargin < 2
-  CurrentNameMat = load('/home/arash/Documents/Software/repositories/neurobit/data/dataset/hsi/results/1931/categorisation/arash/CurrentNamesNatural.mat');
-  CurrentNames = CurrentNameMat.CurrentNames;
-end
-
 DiffColourName = CurrentNames(MetamerPairs(:, 1), :) - CurrentNames(MetamerPairs(:, 2), :);
 
 MetamerColourNameReport = sum(abs(DiffColourName), 2) > 0;
