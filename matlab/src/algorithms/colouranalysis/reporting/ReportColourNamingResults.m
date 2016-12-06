91d4811071cf8773d643e39d3f9fa96ac091c1c8
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-12-06 11:33:09 +0100

    first draft of colour naming metamer report

--- matlab/src/algorithms/colouranalysis/reporting/ReportColourNamingResults.m
+++ matlab/src/algorithms/colouranalysis/reporting/ReportColourNamingResults.m
@@ -1,9 +1,11 @@
-function MetamerColourNameReport = ReportColourNamingResults(MetamerPairs)
+function MetamerColourNameReport = ReportColourNamingResults(MetamerPairs, CurrentNames)
 %ReportColourNamingResults Summary of this function goes here
 %   Detailed explanation goes here
 
-CurrentNameMat = load('/home/arash/Documents/Software/repositories/neurobit/data/dataset/hsi/results/1931/categorisation/arash/CurrentNamesNatural.mat');
-CurrentNames = CurrentNameMat.CurrentNames;
+if nargin < 2
+  CurrentNameMat = load('/home/arash/Documents/Software/repositories/neurobit/data/dataset/hsi/results/1931/categorisation/arash/CurrentNamesNatural.mat');
+  CurrentNames = CurrentNameMat.CurrentNames;
+end
 
 DiffColourName = CurrentNames(MetamerPairs(:, 1), :) - CurrentNames(MetamerPairs(:, 2), :);
 
