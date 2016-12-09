d0cec2dcb2d24d91b57ec47ab590a692c0f58843
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-12-09 15:11:04 +0100

    Added colour categorisation processing to the ApplyMetamerAllIllums.m. Handling the default result folder. Passing the previously computed results to the colour difference function.

--- matlab/src/algorithms/colouranalysis/datareading/ApplyMetamerAllIllums.m
+++ matlab/src/algorithms/colouranalysis/datareading/ApplyMetamerAllIllums.m
@@ -2,23 +2,27 @@ function [ ] = ApplyMetamerAllIllums(ResultFolder)
 %ApplyMetamerAllIllums Summary of this function goes here
 %   Detailed explanation goes here
 
+FunctionPath = mfilename('fullpath');
+[~, FunctionName, ~] = fileparts(FunctionPath);
+FunctionRelativePath = ['matlab', filesep, 'src', filesep, 'algorithms', filesep, 'colouranalysis', filesep, 'datareading', filesep, FunctionName];
+
+GenDataPath = ['data', filesep, 'dataset', filesep, 'hsi', filesep];
+MatDataPath = ['matlab', filesep, 'data', filesep, 'mats', filesep, 'hsi', filesep];
+
 if nargin < 1
-  ResultFolder = '/home/arash/Documents/Software/repositories/neurobit/data/dataset/hsi/results/1931';
+  ResultFolder = strrep(FunctionPath, FunctionRelativePath, [GenDataPath, 'results', filesep, '1931']);
 end
 
-FunctionPath = mfilename('fullpath');
-FunctionRelativePath = 'matlab/src/algorithms/colouranalysis/datareading/ApplyMetamerAllIllums';
-
-ArtLightsPath = strrep(FunctionPath, FunctionRelativePath, 'data/dataset/hsi/lights/ArtLights.mat');
+ArtLightsPath = strrep(FunctionPath, FunctionRelativePath, [GenDataPath, 'lights', filesep, 'ArtLights.mat']);
 ArtIlluminantsMat = load(ArtLightsPath);
 
-IlluminantstPath = strrep(FunctionPath, FunctionRelativePath, 'matlab/data/mats/hsi/illuminants.mat');
+IlluminantstPath = strrep(FunctionPath, FunctionRelativePath, [MatDataPath, 'illuminants.mat']);
 IlluminantsMat = load(IlluminantstPath);
 
-FosterPath = strrep(FunctionPath, FunctionRelativePath, 'matlab/data/mats/hsi/FosterIlluminants.mat');
+FosterPath = strrep(FunctionPath, FunctionRelativePath, [MatDataPath, 'FosterIlluminants.mat']);
 FosterIlluminantsMat = load(FosterPath);
 
-DayLightsPath = strrep(FunctionPath, FunctionRelativePath, 'data/dataset/hsi/lights/DayLights.mat');
+DayLightsPath = strrep(FunctionPath, FunctionRelativePath, [GenDataPath, 'lights', filesep, 'DayLights.mat']);
 DayIlluminantsMat = load(DayLightsPath);
 
 for i = 1:4
@@ -39,16 +43,32 @@ end
 
 function [] = ApplyMetamerTest(wavelength, spectra, FilePath, FileName)
 
+disp(['Illumuninat ', FileName, ' is being processed.']);
+
+MatResPath = [FilePath, filesep, 'metamers', filesep, FileName];
+if exist(MatResPath, 'file')
+  OldComptMat = load(MatResPath);
+  OldComptMat = double(OldComptMat.CompMat);
+else
+  OldComptMat = [];
+end
+
 illuminant.wavelength = wavelength;
 illuminant.spectra = spectra;
-[CompMat, lab] = ColourDiffSpectraSamples([], illuminant);
+[CompMat, lab] = ColourDiffSpectraSamples([], illuminant, OldComptMat);
 CompMat = single(triu(CompMat)); %#ok
-save([FilePath, '/metamers/', FileName], 'CompMat');
+save(MatResPath, 'CompMat');
 
 car = reshape(lab.car, size(lab.car, 1), 3);
 pol = cart2pol3(car(:, [2, 3, 1])); %#ok
-wp = lab.wp; %#ok
+wp = lab.wp;
 
-save([FilePath, '/lab/', FileName], 'car', 'pol', 'wp');
+save([FilePath, filesep, 'lab', filesep, FileName], 'car', 'pol', 'wp');
+
+rgb = lab2rgb(reshape(car, size(car, 1), 1, 3), 'WhitePoint', wp);
+rgb = uint8(min(max(rgb, 0), 1) .* 255);
+[~, CurrentNames] = ColourNamingTestImage(rgb, 'ourlab', false); %#ok
+
+save([FilePath, filesep, 'categorisation', filesep, 'arash', filesep, FileName], 'CurrentNames');
 
 end
