36d2c3bf3479fa16ad463a8b387520f3a91430ef
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-12-13 15:34:08 +0100

    Crossover of two signals.

---
+++ matlab/src/algorithms/colouranalysis/CorssOverSpectra.m
@@ -0,0 +1,13 @@
+function [xi, yi] = CorssOverSpectra(spectra1, wavelength1, spectra2, wavelength2)
+%CorssOverSpectra  find cross overs of two signals
+
+signal1.spectra = spectra1;
+signal1.wavelength = wavelength1;
+signal2.spectra = spectra2;
+signal2.wavelength = wavelength2;
+
+[signal1, signal2] = IntersectIlluminantColourReceptors(signal1, signal2);
+
+[xi, yi] = polyxpoly(signal1.wavelength, signal1.spectra, signal2.wavelength, signal2.spectra);
+
+end
