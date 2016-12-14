2f10ced8bd1e7b0bed9950c651993af47a411d06
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-12-14 10:10:42 +0100

    Added the robust parameter to the polyxpoly.

--- matlab/src/algorithms/colouranalysis/CorssOverSpectra.m
+++ matlab/src/algorithms/colouranalysis/CorssOverSpectra.m
@@ -1,5 +1,9 @@
-function [xi, yi] = CorssOverSpectra(spectra1, wavelength1, spectra2, wavelength2)
+function [xi, yi] = CorssOverSpectra(spectra1, wavelength1, spectra2, wavelength2, robust)
 %CorssOverSpectra  find cross overs of two signals
+
+if nargin < 5
+  robust = false;
+end
 
 signal1.spectra = spectra1;
 signal1.wavelength = wavelength1;
@@ -8,6 +12,6 @@ signal2.wavelength = wavelength2;
 
 [signal1, signal2] = IntersectIlluminantColourReceptors(signal1, signal2);
 
-[xi, yi] = polyxpoly(signal1.wavelength, signal1.spectra, signal2.wavelength, signal2.spectra);
+[xi, yi] = polyxpoly(signal1.wavelength, signal1.spectra, signal2.wavelength, signal2.spectra, robust);
 
 end
--- matlab/src/geometry/polyxpoly.m
+++ matlab/src/geometry/polyxpoly.m
@@ -1,6 +1,12 @@
 % Download intersections.m from http://www.mathworks.com/matlabcentral/fileexchange/11837-fast-and-robust-curve-intersections
 % and put it in a folder on your MATLAB path. Put this function on your path too.
 
-function [xi, yi] = polyxpoly(x1, y1, x2, y2)
-[xi, yi] = intersections(x1, y1, x2, y2);
+function [xi, yi] = polyxpoly(x1, y1, x2, y2, robust)
+
+if nargin < 5
+  robust = true;
+end
+
+[xi, yi] = intersections(x1, y1, x2, y2, robust);
+
 end
