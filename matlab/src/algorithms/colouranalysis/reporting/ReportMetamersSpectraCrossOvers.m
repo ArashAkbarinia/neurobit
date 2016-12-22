960246f70e3533e7e009e76a36b47606550257d3
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-12-22 15:39:53 +0100

    Support for custom datasets.

--- matlab/src/algorithms/colouranalysis/reporting/ReportMetamersSpectraCrossOvers.m
+++ matlab/src/algorithms/colouranalysis/reporting/ReportMetamersSpectraCrossOvers.m
@@ -1,6 +1,10 @@
-function CrossOversReport = ReportMetamersSpectraCrossOvers(MetamerReport)
+function CrossOversReport = ReportMetamersSpectraCrossOvers(MetamerReport, WhichData)
 %ReportMetamersSpectraCrossOvers Summary of this function goes here
 %   Detailed explanation goes here
+
+if nargin < 2
+  WhichData = [];
+end
 
 FunctionPath = mfilename('fullpath');
 [~, FunctionName, ~] = fileparts(FunctionPath);
@@ -11,13 +15,37 @@ DataPath = ['matlab', filesep, 'data', filesep, 'mats', filesep, 'hsi', filesep]
 AllSpectraPath = strrep(FunctionPath, FunctionRelativePath, [DataPath, 'AllSpectraUniformed.mat']);
 AllSpectraMat = load(AllSpectraPath);
 
-spectras = AllSpectraMat.spectra;
-wavelengths = AllSpectraMat.wavelength;
-clear AllSpectraMat;
-
 ThresholdNames = fieldnames(MetamerReport.all.lths);
 nLowThreshes = numel(ThresholdNames);
 nHighThreshes = numel(fieldnames(MetamerReport.all.lths.th1.uths));
+
+% handling which data
+if isempty(WhichData)
+  SpectraInds = 1:MetamerReport.all.NumElements;
+else
+  CatNames = fieldnames(AllSpectraMat.AllSpectra.originals);
+  ncategories = numel(CatNames);
+  CatEls = zeros(ncategories, 1);
+  for i = 1:ncategories
+    CatEls(i) = size(AllSpectraMat.AllSpectra.originals.(CatNames{i}), 1);
+  end
+  CatEls = cumsum(CatEls);
+  SpectraInds = [];
+  
+  % only the selected datasets
+  for i = 1:numel(WhichData)
+    name = WhichData{i};
+    [~, NameInd] = ismember(name, CatNames);
+    if NameInd == 1
+      StartInd = 1;
+    else
+      StartInd = CatEls(NameInd - 1, 1) + 1;
+    end
+    SpectraInds = [SpectraInds, StartInd:CatEls(NameInd, 1)]; %#ok
+  end
+  
+  SpectraInds = SpectraInds';
+end
 
 TmpReport = struct();
 AllCrossOvers = [];
@@ -27,7 +55,8 @@ for i = 1:nLowThreshes
   for j = 1:nHighThreshes
     HighThreshold = LowThreshold.uths.(['uth', num2str(j)]);
     MetamerPairs = HighThreshold.metamerpairs;
-    [xi, yi] = ComputeCorssOverPerPair(MetamerPairs, spectras, wavelengths);
+    MetamerPairs = MetamerPairs(any(ismember(MetamerPairs, SpectraInds), 2), :);
+    [xi, yi] = ComputeCorssOverPerPair(MetamerPairs, AllSpectraMat.spectra, AllSpectraMat.wavelength);
     TmpReport.(ThresholdNames{i}).uths.(['uth', num2str(j)]).uth = HighThreshold.uth;
     TmpReport.(ThresholdNames{i}).uths.(['uth', num2str(j)]).crossovers = [xi, yi];
     AllCrossOvers = [AllCrossOvers; xi, yi]; %#ok
