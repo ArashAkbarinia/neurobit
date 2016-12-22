a55247a5a8f5d8d73d1f34189eb2213f96481101
Arash Akbarinia <akbarinia.arash@gmail.com>
2016-12-22 10:58:52 +0100

    Added all illuminants uniformed. Computing the crossovers.

--- matlab/src/algorithms/colouranalysis/reporting/ReportMetamersSpectraCrossOvers.m
+++ matlab/src/algorithms/colouranalysis/reporting/ReportMetamersSpectraCrossOvers.m
@@ -1,14 +1,14 @@
-function [xs, ys] = PlotSpectraCrossOvers(MetamerReport)
-%PlotSpectraCrossOvers Summary of this function goes here
+function CrossOversReport = ReportMetamersSpectraCrossOvers(MetamerReport)
+%ReportMetamersSpectraCrossOvers Summary of this function goes here
 %   Detailed explanation goes here
 
 FunctionPath = mfilename('fullpath');
 [~, FunctionName, ~] = fileparts(FunctionPath);
-FunctionRelativePath = ['matlab', filesep, 'src', filesep, 'algorithms', filesep, 'colouranalysis', filesep, 'plotting', filesep, FunctionName];
+FunctionRelativePath = ['matlab', filesep, 'src', filesep, 'algorithms', filesep, 'colouranalysis', filesep, 'reporting', filesep, FunctionName];
 
 DataPath = ['matlab', filesep, 'data', filesep, 'mats', filesep, 'hsi', filesep];
 
-AllSpectraPath = strrep(FunctionPath, FunctionRelativePath, [DataPath, 'AllSpectraNormalised.mat']);
+AllSpectraPath = strrep(FunctionPath, FunctionRelativePath, [DataPath, 'AllSpectraUniformed.mat']);
 AllSpectraMat = load(AllSpectraPath);
 
 spectras = AllSpectraMat.spectra;
@@ -17,23 +17,25 @@ clear AllSpectraMat;
 
 ThresholdNames = fieldnames(MetamerReport.all.lths);
 nLowThreshes = numel(ThresholdNames);
-
 nHighThreshes = numel(fieldnames(MetamerReport.all.lths.th1.uths));
 
-xs = [];
-ys = [];
-
-% TODO: too slow, fix it
+TmpReport = struct();
+AllCrossOvers = [];
 for i = 1:nLowThreshes
   LowThreshold = MetamerReport.all.lths.(ThresholdNames{i});
+  TmpReport.(ThresholdNames{i}).lth = LowThreshold.lth;
   for j = 1:nHighThreshes
-    HighThreshold = LowThreshold.uths;
-    MetamerPairs = HighThreshold.(['uth', num2str(j)]).metamerpairs;
+    HighThreshold = LowThreshold.uths.(['uth', num2str(j)]);
+    MetamerPairs = HighThreshold.metamerpairs;
     [xi, yi] = ComputeCorssOverPerPair(MetamerPairs, spectras, wavelengths);
-    xs = [xs; xi]; %#ok
-    ys = [ys; yi]; %#ok
+    TmpReport.(ThresholdNames{i}).uths.(['uth', num2str(j)]).uth = HighThreshold.uth;
+    TmpReport.(ThresholdNames{i}).uths.(['uth', num2str(j)]).crossovers = [xi, yi];
+    AllCrossOvers = [AllCrossOvers; xi, yi]; %#ok
   end
 end
+
+CrossOversReport.all.lths = TmpReport;
+CrossOversReport.all.crossovers = AllCrossOvers;
 
 end
 
@@ -48,12 +50,10 @@ for i = 1:size(MetamerPairs, 1)
   % fetching the spectra
   spectra1 = spectras{row};
   spectra1 = reshape(spectra1, size(spectra1, 3), 1);
-  spectra1 = spectra1 ./ max(spectra1);
   spectra2 = spectras{col};
   spectra2 = reshape(spectra2, size(spectra2, 3), 1);
-  spectra2 = spectra2 ./ max(spectra2);
   
-  [xi, yi] = CorssOverSpectra(spectra1, wavelengths{row}, spectra2, wavelengths{col});
+  [xi, yi] = CorssOverSpectra(spectra1, wavelengths{row}, spectra2, wavelengths{col}, true);
   xs = [xs; xi]; %#ok
   ys = [ys; yi]; %#ok
 end
