56a7df5cdd677d9933668cf483e8b0a7b16de087
Arash Akbarinia <akbarinia.arash@gmail.com>
2017-01-05 14:04:43 +0100

    Handling exlucindg as well.

--- matlab/src/algorithms/colouranalysis/datareading/ExtractDatasetIndices.m
+++ matlab/src/algorithms/colouranalysis/datareading/ExtractDatasetIndices.m
@@ -1,23 +1,42 @@
-function SpectraInds = ExtractDatasetIndices(MetamerReport, AllSpectra, WhichData)
+function SpectraInds = ExtractDatasetIndices(MetamerReport, AllSpectra, WhichData, isinclude)
 %ExtractDatasetIndices  extracts the indices of specified dataset
 %
 % inputs
 %   MetamerReport  the metamer report.
 %   AllSpectra     original spectra reflectance datasets.
 %   WhichData      the name of desired datasets in a cell array.
+%   isinclude      whether the datasets to be included or excluded.
 %
 % outputs
 %   SpectraInds  indices of the specifid datasets.
 %
 
+if nargin < 4
+  isinclude = true;
+end
+
 if isempty(WhichData) || strcmpi('all', WhichData{1})
   SpectraInds = 1:MetamerReport.NumElements;
 else
   CatNames = fieldnames(AllSpectra.originals);
+  
+  % create the exclude list
+  if ~isinclude
+    ExcludeList = WhichData;
+    WhichData = {};
+  end
+  
   ncategories = numel(CatNames);
   CatEls = zeros(ncategories, 1);
   for i = 1:ncategories
     CatEls(i) = size(AllSpectra.originals.(CatNames{i}), 1);
+    % the case of exclude
+    if ~isinclude
+      [~, NameInd] = ismember(CatNames{i}, ExcludeList);
+      if NameInd == 0
+        WhichData{end + 1} = CatNames{i}; %#ok
+      end
+    end
   end
   CatEls = cumsum(CatEls);
   SpectraInds = [];
