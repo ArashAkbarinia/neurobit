9ada221c3e2d3a67670e1265e6cce23640af6e14
Arash Akbarinia <akbarinia.arash@gmail.com>
2017-01-06 11:10:00 +0100

    Plotting the histogram of colours in polar coordinates.

--- matlab/src/algorithms/colouranalysis/plotting/PlotMetamerPairsUnderTwoIllumsHist.m
+++ matlab/src/algorithms/colouranalysis/plotting/PlotMetamerPairsUnderTwoIllumsHist.m
@@ -1,22 +1,67 @@
-function PlotMetamerPairsUnderTwoIllumsHist(lab11, lab21, lab12, lab22, minds, IllumNames)
+function ShiftStats = PlotMetamerPairsUnderTwoIllumsHist(lab11, lab21, lab12, lab22, minds, IllumNames, OrgHist1, OrgHist2)
+
+t1 = OrgHist1.theta;
+r1 = OrgHist1.RatRadius;
+t2 = OrgHist2.theta;
+r2 = OrgHist2.RatRadius;
 
 pol11 = cart2pol3(lab11(minds, [2, 3, 1]));
 pol21 = cart2pol3(lab21(minds, [2, 3, 1]));
 pol12 = cart2pol3(lab12(minds, [2, 3, 1]));
 pol22 = cart2pol3(lab22(minds, [2, 3, 1]));
 
-nbins = 20;
+% for title compute the radius and theta differences
+t1d = pdist2(pol11(:, 1)', pol12(:, 1)', @PdistAbsAngDiff);
+t1d = num2str(rad2deg(t1d));
+r1d = pdist2(pol11(:, 2)', pol12(:, 2)');
+r1d = num2str(r1d);
+t2d = pdist2(pol21(:, 1)', pol22(:, 1)', @PdistAbsAngDiff);
+t2d = num2str(rad2deg(t2d));
+r2d = pdist2(pol21(:, 2)', pol22(:, 2)');
+r2d = num2str(r2d);
+
+% compute the shifts
+ShiftBins = 180;
+t1s = AngDiff(pol11(:, 1), pol12(:, 1));
+r1s = pol11(:, 2) - pol12(:, 2);
+t2s = AngDiff(pol21(:, 1), pol22(:, 1));
+r2s = pol21(:, 2) - pol22(:, 2);
+
+ShiftStats.t1s = t1s;
+ShiftStats.r1s = r1s;
+ShiftStats.t2s = t2s;
+ShiftStats.r2s = r2s;
+
+% compute the histograms
+nbins = length(r1) / 4;
 [t11, r11] = rose(pol11(:, 1), nbins);
 r11 = r11 ./ sum(r11);
+r11 = r11 ./ r1;
+r11(isnan(r11) | isinf(r11)) = 0;
 
 [t21, r21] = rose(pol21(:, 1), nbins);
 r21 = r21 ./ sum(r21);
+r21 = r21 ./ r2;
+r21(isnan(r21) | isinf(r21)) = 0;
 
 [t12, r12] = rose(pol12(:, 1), nbins);
 r12 = r12 ./ sum(r12);
+r12 = r12 ./ r1;
+r12(isnan(r12) | isinf(r12)) = 0;
 
 [t22, r22] = rose(pol22(:, 1), nbins);
 r22 = r22 ./ sum(r22);
+r22 = r22 ./ r2;
+r22(isnan(r22) | isinf(r22)) = 0;
+
+ShiftStats.r11 = r11;
+ShiftStats.t11 = t11;
+ShiftStats.r12 = r12;
+ShiftStats.t12 = t12;
+ShiftStats.r21 = r21;
+ShiftStats.t21 = t21;
+ShiftStats.r22 = r22;
+ShiftStats.t22 = t22;
 
 MaxAll(1) = max(r11);
 MaxAll(2) = max(r21);
@@ -31,24 +76,22 @@ subplot(rows, cols, 1);
 DrawHueCircle(MaxRadius);
 hold on;
 polar(t11, r11, 'black');
-title(['Pair 1 ''', IllumNames{1}, '''']);
+polar(t12, r12, '--white');
+title(['P1 (black) P2(white) ''', IllumNames{1}, '''', ' rd=[', r1d,']', ' td=[', t1d,']']);
 
-subplot(rows, cols, 2);
-DrawHueCircle(MaxRadius);
-hold on;
-polar(t12, r12, 'black');
-title(['Pair 2 ''', IllumNames{1}, '''']);
+h = subplot(rows, cols, 2);
+WindRose(rad2deg(t1s), r1s, 'parent', h, 'cmap', gray, 'n', ShiftBins)
+title(['Theta and radius shift ''', IllumNames{1}, '''', ' rd=[', r1d,']', ' td=[', t1d,']']);
 
 subplot(rows, cols, 3);
 DrawHueCircle(MaxRadius);
 hold on;
 polar(t21, r21, 'black');
-title(['Pair 1 ''', IllumNames{2}, '''']);
+polar(t22, r22, '--white');
+title(['P1 (black) P2(white) ''', IllumNames{2}, '''', ' rd=[', r2d,']', ' td=[', t2d,']']);
 
-subplot(rows, cols, 4);
-DrawHueCircle(MaxRadius);
-hold on;
-polar(t22, r22, 'black');
-title(['Pair 2 ''', IllumNames{2}, '''']);
+h = subplot(rows, cols, 4);
+WindRose(rad2deg(t2s), r2s, 'parent', h, 'cmap', gray, 'n', ShiftBins)
+title(['Theta and radius shift ''', IllumNames{2}, '''', ' rd=[', r2d,']', ' td=[', t2d,']']);
 
 end
---
+++ matlab/src/algorithms/colouranalysis/plotting/PlotMetamersHueChange.m
@@ -0,0 +1,177 @@
+function IllumPairsShiftStats = PlotMetamersHueChange(ResultsFolder, LabFolder, saveme, WhichLth, WhichUth, ExcludeDatasets)
+%PlotMetamersHueChange Summary of this function goes here
+%   Detailed explanation goes here
+
+FunctionPath = mfilename('fullpath');
+[~, FunctionName, ~] = fileparts(FunctionPath);
+FunctionRelativePath = ['matlab', filesep, 'src', filesep, 'algorithms', filesep, 'colouranalysis', filesep, 'plotting', filesep, FunctionName];
+
+GenDataPath = ['data', filesep, 'dataset', filesep, 'hsi', filesep];
+
+if nargin < 6 || isempty(ExcludeDatasets)
+  ExcludeDatasets = [];
+end
+if nargin < 5
+  WhichUth = [];
+end
+if nargin < 4
+  WhichLth = [];
+end
+if nargin < 3
+  saveme = false;
+end
+if nargin < 2 || isempty(LabFolder)
+  LabFolder = strrep(FunctionPath, FunctionRelativePath, [GenDataPath, 'results', filesep, '1931', filesep, 'lab', filesep]);
+end
+LabMats = struct();
+
+SubFolders = GetSubFolders(ResultsFolder);
+nCombinations = numel(SubFolders);
+c = -nCombinations * 2;
+nIllums = (1 + sqrt(1 - 4 * c)) / 2;
+
+MetamerReportMat = load([ResultsFolder, filesep, SubFolders{1}, filesep, 'AllIlluminantReport']);
+MetamerReport = MetamerReportMat.MetamerReport;
+
+ThresholdNames = fieldnames(MetamerReport.all.lths);
+nLowThreshes = numel(ThresholdNames);
+nHighThreshes = numel(fieldnames(MetamerReport.all.lths.th1.uths));
+
+nbins = 180;
+
+% getting the AllSpectra
+FunctionPath = mfilename('fullpath');
+[~, FunctionName, ~] = fileparts(FunctionPath);
+FunctionRelativePath = ['matlab', filesep, 'src', filesep, 'algorithms', filesep, 'colouranalysis', filesep, 'plotting', filesep, FunctionName];
+
+DataPath = ['matlab', filesep, 'data', filesep, 'mats', filesep, 'hsi', filesep];
+
+AllSpectraPath = strrep(FunctionPath, FunctionRelativePath, [DataPath, 'AllSpectraUniformed.mat']);
+AllSpectraMat = load(AllSpectraPath);
+
+% output
+IllumPairsShiftStats = cell(nIllums, nIllums);
+IllumNamesOrder = cell(nIllums, 1);
+LastIllumIndx = 1;
+
+for s = 1:nCombinations
+  CurrentSubFolder = [ResultsFolder, filesep, SubFolders{s}, filesep];
+  if ~exist([CurrentSubFolder, 'AllIlluminantReport.mat'], 'file')
+    disp([CurrentSubFolder, 'AllIlluminantReport. mat']);
+    continue;
+  end
+  disp(['Processing: ', SubFolders{s}]);
+  
+  MetamerReportMat = load([CurrentSubFolder, 'AllIlluminantReport.mat']);
+  MetamerReport = MetamerReportMat.MetamerReport;
+  
+  % handling which data
+  SpectraInds = ExtractDatasetIndices(MetamerReport.all, AllSpectraMat.AllSpectra, ExcludeDatasets, true);
+  
+  LabCars11 = [];
+  LabCars12 = [];
+  LabCars21 = [];
+  LabCars22 = [];
+  
+  % checking to see if the illuminant exist
+  CurrentIllums = MetamerReport.illuminants;
+  
+  il1 = find(strcmp(IllumNamesOrder, CurrentIllums{1}));
+  if isempty(il1)
+    LabMats.(CurrentIllums{1}) = load([LabFolder, CurrentIllums{1}]);
+    LabHist.(CurrentIllums{1}) = OriginalDataLabHist(LabMats.(CurrentIllums{1}).pol(SpectraInds, :), nbins);
+    IllumNamesOrder{LastIllumIndx} = CurrentIllums{1};
+    il1 = LastIllumIndx;
+    LastIllumIndx = LastIllumIndx + 1;
+  end
+  il2 = find(strcmp(IllumNamesOrder, CurrentIllums{2}));
+  if isempty(il2)
+    LabMats.(CurrentIllums{2}) = load([LabFolder, CurrentIllums{2}]);
+    LabHist.(CurrentIllums{2}) = OriginalDataLabHist(LabMats.(CurrentIllums{2}).pol, nbins);
+    IllumNamesOrder{LastIllumIndx} = CurrentIllums{2};
+    il2 = LastIllumIndx;
+    LastIllumIndx = LastIllumIndx + 1;
+  end
+  
+  for i = 1:nLowThreshes
+    LowThreshold = MetamerReport.all.lths.(ThresholdNames{i});
+    if isempty(WhichLth) || ismember(LowThreshold.lth, WhichLth)
+      for j = 1:nHighThreshes
+        HighThreshold = LowThreshold.uths.(['uth', num2str(j)]);
+        if isempty(WhichUth) || ismember(HighThreshold.uth, WhichUth)
+          
+          mpairs = HighThreshold.metamerpairs;
+          mpairs = mpairs(any(ismember(mpairs, SpectraInds), 2), :);
+          LabCars11 = [LabCars11; LabMats.(CurrentIllums{1}).car(mpairs(:, 1), :)]; %#ok
+          LabCars12 = [LabCars12; LabMats.(CurrentIllums{1}).car(mpairs(:, 2), :)]; %#ok
+          LabCars21 = [LabCars21; LabMats.(CurrentIllums{2}).car(mpairs(:, 1), :)]; %#ok
+          LabCars22 = [LabCars22; LabMats.(CurrentIllums{2}).car(mpairs(:, 2), :)]; %#ok
+        end
+      end
+    end
+  end
+  
+  ShiftStats = PlotLabCarsOnHueCircle(LabCars11, LabCars12, LabCars21, LabCars22, CurrentIllums, CurrentSubFolder, LabHist.(CurrentIllums{1}), LabHist.(CurrentIllums{2}), saveme);
+  IllumPairsShiftStats{il1, il2} = ShiftStats;
+  IllumPairsShiftStats{il2, il1} = ShiftStats;
+end
+
+end
+
+function LabHist = OriginalDataLabHist(PolLab, nbins)
+
+if nargin < 2
+  nbins = 12;
+end
+[theta, radius] = rose(PolLab(:, 1), nbins);
+
+LabHist.AbsRadius = radius;
+LabHist.RatRadius = radius ./ sum(radius);
+LabHist.theta = theta;
+
+end
+
+function ShiftStats = PlotLabCarsOnHueCircle(LabCars11, LabCars12, LabCars21, LabCars22, CurrentIllums, SubFolders, OrgHist1, OrgHist2, saveme)
+
+ShiftStats = struct();
+
+if isempty(LabCars11)
+  return;
+end
+
+DeltaE2000(:, 1) = deltae2000(LabCars11, LabCars12);
+DeltaE2000(:, 2) = deltae2000(LabCars21, LabCars22);
+
+m1n2 = DeltaE2000(:, 1) < 0.5;
+m2n1 = DeltaE2000(:, 2) < 0.5;
+
+FigPos = [0, 0, 50, 25];
+if saveme
+  isvisible = 'off';
+else
+  isvisible = 'on';
+end
+
+if any(m1n2)
+  FigureName = ['metamers under ''', CurrentIllums{1}, ''' but not under ''', CurrentIllums{2}, ''''];
+  FigureHandler = figure('name', FigureName, 'PaperPosition', FigPos, 'visible', isvisible);
+  ShiftStats.m1n2 = PlotMetamerPairsUnderTwoIllumsHist(LabCars11, LabCars21, LabCars12, LabCars22, m1n2, CurrentIllums, OrgHist1, OrgHist2);
+  suptitle(FigureName);
+  if saveme
+    print(FigureHandler, [SubFolders, 'm1n2.jpg'], '-djpeg', '-r0');
+    close(FigureHandler);
+  end
+end
+
+if any(m2n1)
+  FigureName = ['metamers under ''', CurrentIllums{2}, ''' but not under ''', CurrentIllums{1}, ''''];
+  FigureHandler = figure('name', FigureName, 'PaperPosition', FigPos, 'visible', isvisible);
+  ShiftStats.m2n1 = PlotMetamerPairsUnderTwoIllumsHist(LabCars11, LabCars21, LabCars12, LabCars22, m2n1, CurrentIllums, OrgHist1, OrgHist2);
+  suptitle(FigureName);
+  if saveme
+    print(FigureHandler, [SubFolders, 'm2n1.jpg'], '-djpeg', '-r0');
+    close(FigureHandler);
+  end
+end
+
+end
