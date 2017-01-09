91b6f7b779d51fe2525ddae6d1f462dbb7b41913
Arash Akbarinia <akbarinia.arash@gmail.com>
2017-01-09 10:29:17 +0100

    Saving statistics of shifts in the output results.

--- matlab/src/algorithms/colouranalysis/plotting/PlotMetamerPairsUnderTwoIllumsHist.m
+++ matlab/src/algorithms/colouranalysis/plotting/PlotMetamerPairsUnderTwoIllumsHist.m
@@ -1,9 +1,7 @@
 function ShiftStats = PlotMetamerPairsUnderTwoIllumsHist(lab11, lab21, lab12, lab22, minds, IllumNames, OrgHist1, OrgHist2)
 
-t1 = OrgHist1.theta;
-r1 = OrgHist1.RatRadius;
-t2 = OrgHist2.theta;
-r2 = OrgHist2.RatRadius;
+r1 = OrgHist1.AbsRadius;
+r2 = OrgHist2.AbsRadius;
 
 pol11 = cart2pol3(lab11(minds, [2, 3, 1]));
 pol21 = cart2pol3(lab21(minds, [2, 3, 1]));
@@ -27,6 +25,7 @@ r1s = pol11(:, 2) - pol12(:, 2);
 t2s = AngDiff(pol21(:, 1), pol22(:, 1));
 r2s = pol21(:, 2) - pol22(:, 2);
 
+% saving the shifts in the output
 ShiftStats.t1s = t1s;
 ShiftStats.r1s = r1s;
 ShiftStats.t2s = t2s;
@@ -35,25 +34,11 @@ ShiftStats.r2s = r2s;
 % compute the histograms
 nbins = length(r1) / 4;
 [t11, r11] = rose(pol11(:, 1), nbins);
-r11 = r11 ./ sum(r11);
-r11 = r11 ./ r1;
-r11(isnan(r11) | isinf(r11)) = 0;
-
 [t21, r21] = rose(pol21(:, 1), nbins);
-r21 = r21 ./ sum(r21);
-r21 = r21 ./ r2;
-r21(isnan(r21) | isinf(r21)) = 0;
-
 [t12, r12] = rose(pol12(:, 1), nbins);
-r12 = r12 ./ sum(r12);
-r12 = r12 ./ r1;
-r12(isnan(r12) | isinf(r12)) = 0;
-
 [t22, r22] = rose(pol22(:, 1), nbins);
-r22 = r22 ./ sum(r22);
-r22 = r22 ./ r2;
-r22(isnan(r22) | isinf(r22)) = 0;
 
+% saving the absolute hist counts in the output
 ShiftStats.r11 = r11;
 ShiftStats.t11 = t11;
 ShiftStats.r12 = r12;
@@ -63,17 +48,32 @@ ShiftStats.t21 = t21;
 ShiftStats.r22 = r22;
 ShiftStats.t22 = t22;
 
-MaxAll(1) = max(r11);
-MaxAll(2) = max(r21);
-MaxAll(3) = max(r12);
-MaxAll(4) = max(r22);
-MaxRadius = max(MaxAll);
+% normalising the histograms for visualisation
+r11 = r11 ./ r1;
+r11(isnan(r11) | isinf(r11)) = 0;
+
+r21 = r21 ./ r2;
+r21(isnan(r21) | isinf(r21)) = 0;
+
+r12 = r12 ./ r1;
+r12(isnan(r12) | isinf(r12)) = 0;
+
+r22 = r22 ./ r2;
+r22(isnan(r22) | isinf(r22)) = 0;
+
+% computing the maximu radius
+MaxAll1(1) = max(r11);
+MaxAll1(2) = max(r12);
+MaxAll2(1) = max(r21);
+MaxAll2(2) = max(r22);
+MaxRadius1 = max(MaxAll1);
+MaxRadius2 = max(MaxAll2);
 
 cols = 2;
 rows = 2;
 
 subplot(rows, cols, 1);
-DrawHueCircle(MaxRadius);
+DrawHueCircle(MaxRadius1);
 hold on;
 polar(t11, r11, 'black');
 polar(t12, r12, '--white');
@@ -84,7 +84,7 @@ WindRose(rad2deg(t1s), r1s, 'parent', h, 'cmap', gray, 'n', ShiftBins)
 title(['Theta and radius shift ''', IllumNames{1}, '''', ' rd=[', r1d,']', ' td=[', t1d,']']);
 
 subplot(rows, cols, 3);
-DrawHueCircle(MaxRadius);
+DrawHueCircle(MaxRadius2);
 hold on;
 polar(t21, r21, 'black');
 polar(t22, r22, '--white');
